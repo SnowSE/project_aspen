7ccc1643d5292ebb1e0fda8f881906f368bb7402
Kyler Daybell <kyler.daybell96@gmail.com>
2020-03-17 09:02:13 -0600

    this looks like it should work, We will need to put it under test to be sure

--- src/aspen/aspen.web/ClientApp/src/services/APIService.tsx
+++ src/aspen/aspen.web/ClientApp/src/services/APIService.tsx
@@ -4,6 +4,7 @@ import { Charity } from "../models/CharityModel";
 import { IDomainService } from "./IDomainService";
 import { Theme } from "../models/Theme";
 
+const url = process.env.API_URL
 
 export class APIService implements IAPIService {
     IDomainService: IDomainService;
@@ -12,12 +13,12 @@ export class APIService implements IAPIService {
         this.IDomainService = IDomainService
     }
 
-    async GetCharityHomePage(): Promise<CharityHomePage> {
+
+    public async GetCharityHomePage(): Promise<CharityHomePage> {
         let domain = this.IDomainService.GetDomain();
-        let url = ""
         let headers = { "Content-Type": "application/json" };
-        url = url + "/"+ domain;
-        let response = await fetch(url, {
+        let newurl = url + "/getcharitybydomain/"+ domain;
+        let response = await fetch(newurl, {
             method: "GET",
             headers: headers
         })
@@ -39,14 +40,79 @@ export class APIService implements IAPIService {
             // TODO get the theme and place it here. 
             return new CharityHomePage(theme, charityObject)
         }
+        //TODO: make a second api call to get the theme and remove the theme from the first api call 
+
         let theme = new Theme("#438f00","#67cc0e","#FFFFFF", "#608045","Arial");
         let charityObject = new Charity(1,"Kylers penguin's","kyler.com","this is where the awesome penguin's live")
         let charityHomePage = new CharityHomePage(theme,charityObject);
-        return new CharityHomePage(theme, charityObject);
-        
-        
-        
-        
+        return new CharityHomePage(theme, charityObject); 
+    }
+
+
+    public async GetAllCharities(): Promise<Charity[]> {
+        let headers = { "Content-Type": "application/json" };
+        let newurl = url + "/Charity/GetAll"
+        let response = await fetch(newurl, {
+            method: "GET",
+            headers: headers
+        })
+
+        let responseJson = await response.json()
+
+
+        return [new Charity(1,"Kylers penguin's","kyler.com","this is where the awesome penguin's live")]
+    }
+    public async GetCharityByID(charity: Charity): Promise<Charity> {
+        let headers = { "Content-Type": "application/json" };
+        let newurl = url + "/Charity/get/"+charity.ID.toString
+        let response = await fetch(newurl, {
+            method: "GET",
+            headers: headers
+        })
+
+        let responseJson = await response.json();
+
+        let c = new Charity(1,"Kylers penguin's","kyler.com","this is where the awesome penguin's live");
+        return c 
+    }
+    public async PostCreateCharity(charity: Charity): Promise<boolean> {
+        let headers = { "Content-Type": "application/json" };
+        let body = JSON.stringify(charity);
+        let newurl = url + "/Charity/Create"
+        let response = await fetch(newurl, {
+            method: "POST",
+            headers: headers,
+            body: body
+        })
+
+        let responseJson = await response.json();
+        return true;
+    }
+    public async PostUpdateCharity(charity: Charity): Promise<boolean> {
+        let headers = { "Content-Type": "application/json" };
+        let body = JSON.stringify(charity);
+        let newurl = url + "/Charity/Update"
+        let response = await fetch(newurl, {
+            method: "POST",
+            headers: headers,
+            body: body
+        })
+
+        let responseJson = await response.json();
+        return true;
+    }
+    public async PostDeleteCharity(charity: Charity): Promise<boolean> {
+        let headers = { "Content-Type": "application/json" };
+        let body = JSON.stringify(charity);
+        let newurl = url + "/Charity/Update"
+        let response = await fetch(newurl, {
+            method: "POST",
+            headers: headers,
+            body: body
+        })
+
+        let responseJson = await response.json();
+        return true;
     }
 
 }
--- src/aspen/aspen.web/ClientApp/src/services/IAPIService.tsx
+++ src/aspen/aspen.web/ClientApp/src/services/IAPIService.tsx
@@ -1,6 +1,12 @@
 import { Charity } from '../models/CharityModel'
 import { CharityHomePage } from '../models/CharityHomePageModel';
+import { promises } from 'dns';
 
 export interface IAPIService {
     GetCharityHomePage(Charity: Charity): Promise<CharityHomePage>
+    GetAllCharities():Promise<Charity[]>
+    GetCharityByID(Charity: Charity): Promise<Charity>
+    PostCreateCharity(Charity: Charity):Promise<boolean>
+    PostUpdateCharity(Charity:Charity):Promise<boolean>
+    PostDeleteCharity(Charity:Charity):Promise<boolean>
 }
