5ceaf5211db3ddccfbbee1e42dd8fce790732d87
Kyler Daybell <kyler.daybell96@gmail.com>
2020-03-17 09:06:27 -0600

    Updated both the api service and the dummy api serviece to implement the calls to the admin controller should be good to go

--- src/aspen/aspen.web/ClientApp/src/services/DummyAPIService.tsx
+++ src/aspen/aspen.web/ClientApp/src/services/DummyAPIService.tsx
@@ -7,11 +7,32 @@ import {DomainService} from "./DomainService"
 
 export class DummyAPIService implements APIService{
     IDomainService: IDomainService = new DomainService();
-    async GetCharityHomePage(): Promise <CharityHomePage> {
+    public async GetCharityHomePage(): Promise <CharityHomePage> {
         let theme = new Theme("#438f00","#67cc0e","#FFFFFF", "#608045","Arial");
         let charity = new Charity(1,"Kylers penguin's","kyler.com","this is where the awesome penguin's live")
         let charityHomePage = new CharityHomePage(theme,charity);
         
         return charityHomePage;
+    }
+    public async GetAllCharities(): Promise<Charity[]> {
+        let charity = new Charity(1,"Kylers penguin's","kyler.com","this is where the awesome penguin's live")
+        return [charity]
+    }
+    public async GetCharityByID(charity: Charity): Promise<Charity> {
+        if(charity.ID == 1){
+            let charity = new Charity(1,"Kylers penguin's","kyler.com","this is where the awesome penguin's live")
+            return charity
+        }else{
+            return new Charity(0,"","","")
+        }
+    }
+    public async PostCreateCharity(charity: Charity): Promise<boolean> {
+        return true
+    }
+    public async PostUpdateCharity(charity: Charity): Promise<boolean> {
+        return true
+    }
+    public async PostDeleteCharity(charity: Charity): Promise<boolean> {
+        return true
     }
 }
