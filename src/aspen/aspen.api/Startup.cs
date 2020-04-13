7c605d522fe84983af04a9308c92f213f0944b28
Alex Mickelson <alexmickelson96@gmail.com>
2020-04-13 13:18:15 -0600

    Trying to connect to database

--- src/aspen/aspen.api/Startup.cs
+++ src/aspen/aspen.api/Startup.cs
@@ -29,23 +29,23 @@ namespace Aspen.Api
 
         public Startup(IConfiguration configuration)
         {
-            // var passfilePath = Environment.GetEnvironmentVariable("PGPASSFILE");
-            // var connectionStringBuilder = new NpgsqlConnectionStringBuilder();
-            // connectionStringBuilder.Passfile = passfilePath;
+            var passfilePath = Environment.GetEnvironmentVariable("PGPASSFILE");
+            var connectionStringBuilder = new NpgsqlConnectionStringBuilder();
+            connectionStringBuilder.Passfile = passfilePath;
 
-            // var alltext = File.ReadAllText(passfilePath);
-            // var passfile = alltext.Split(":");
+            var alltext = File.ReadAllText(passfilePath);
+            var passfile = alltext.Split(":");
 
-            // connectionStringBuilder.SslMode = SslMode.Require;
-            // connectionStringBuilder.TrustServerCertificate = true;
-            // connectionStringBuilder.Host = passfile[0];
-            // connectionStringBuilder.Port = int.Parse(passfile[1]);
-            // connectionStringBuilder.Database = "Admin";
-            // connectionStringBuilder.Username = passfile[3];
+            connectionStringBuilder.SslMode = SslMode.Require;
+            connectionStringBuilder.TrustServerCertificate = true;
+            connectionStringBuilder.Host = passfile[0];
+            connectionStringBuilder.Port = int.Parse(passfile[1]);
+            connectionStringBuilder.Database = "Admin";
+            connectionStringBuilder.Username = passfile[3];
             // connectionStringBuilder.ClientCertificate = "/app/.postgresql/postgresql.crt";
 
-            // connectionString = new ConnectionString(connectionStringBuilder.ConnectionString + ";");
-            connectionString = new ConnectionString("Host=database; Port=5432; Database=Admin; Username=Aspen; Password=Aspen;"); 
+            connectionString = new ConnectionString(connectionStringBuilder.ConnectionString + ";");
+            // connectionString = new ConnectionString("Host=database; Port=5432; Database=Admin; Username=Aspen; Password=Aspen;"); 
 
             Configuration = configuration;
         }
