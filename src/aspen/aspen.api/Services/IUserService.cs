d7977d775ffe9b913ed0af281f900c0c13ca7c15
Mike <mexvance04@gmail.com>
2020-04-27 15:22:48 -0600

    updated the user service

--- src/aspen/aspen.api/Services/IUserService.cs
+++ src/aspen/aspen.api/Services/IUserService.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using Aspen.Api.Http;
+using Aspen.Core;
 using Aspen.Core.Models;
 
 namespace Aspen.Api.Services
@@ -7,5 +11,7 @@ namespace Aspen.Api.Services
     {
         User Authenticate(string username, string password);
         IEnumerable<User> GetAll();
+        //Task<Result<User>> getValidUser(UserRequest request);
+        Task<Result<User>> GetByUserId(Guid id);
     }
 }
