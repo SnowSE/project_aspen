ffadf8fe570a83178c70769223d872ac2d8f05e7
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2021-10-12 23:45:00 -0600

    created migrations

---
+++ src/api/Migrations/20211013054117_InitialCreate.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Aspen.Api.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Events",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "text", nullable: false),
+                    Date = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    Location = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    PrimaryImageUrl = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Events", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PageData",
+                columns: table => new
+                {
+                    Key = table.Column<string>(type: "text", nullable: false),
+                    Data = table.Column<JsonDocument>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PageData", x => x.Key);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Persons",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "text", nullable: false),
+                    AuthID = table.Column<string>(type: "text", nullable: true),
+                    Name = table.Column<string>(type: "text", nullable: true),
+                    Bio = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Persons", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "text", nullable: false),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    MainImage = table.Column<string>(type: "text", nullable: true),
+                    OwnerID = table.Column<string>(type: "text", nullable: true),
+                    EventID = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Teams_Events_EventID",
+                        column: x => x.EventID,
+                        principalTable: "Events",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Registrations",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "text", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    IsPublic = table.Column<bool>(type: "boolean", nullable: false),
+                    Nickname = table.Column<string>(type: "text", nullable: true),
+                    OwnerID = table.Column<string>(type: "text", nullable: true),
+                    TeamID = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Registrations", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Registrations_Teams_TeamID",
+                        column: x => x.TeamID,
+                        principalTable: "Teams",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersonRegistrations",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "text", nullable: false),
+                    PersonID = table.Column<string>(type: "text", nullable: true),
+                    RegistrationID = table.Column<string>(type: "text", nullable: true),
+                    CreatedDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PersonRegistrations", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_PersonRegistrations_Persons_PersonID",
+                        column: x => x.PersonID,
+                        principalTable: "Persons",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PersonRegistrations_Registrations_RegistrationID",
+                        column: x => x.RegistrationID,
+                        principalTable: "Registrations",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonRegistrations_PersonID",
+                table: "PersonRegistrations",
+                column: "PersonID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PersonRegistrations_RegistrationID",
+                table: "PersonRegistrations",
+                column: "RegistrationID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Registrations_TeamID",
+                table: "Registrations",
+                column: "TeamID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_EventID",
+                table: "Teams",
+                column: "EventID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PageData");
+
+            migrationBuilder.DropTable(
+                name: "PersonRegistrations");
+
+            migrationBuilder.DropTable(
+                name: "Persons");
+
+            migrationBuilder.DropTable(
+                name: "Registrations");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+
+            migrationBuilder.DropTable(
+                name: "Events");
+        }
+    }
+}
