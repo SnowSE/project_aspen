df17245464bab308b68d643128be65a02ccb6e24
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2021-10-18 22:31:30 -0600

    uncommented migrate command on program.cs and changed the db back to local

--- src/api/Startup.cs
+++ src/api/Startup.cs
@@ -82,7 +82,7 @@ namespace Api
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "dotnet", Version = "v1" });
             });
 
-            services.AddDbContext<AspenContext>(options => options.UseNpgsql(Configuration.GetConnectionString("localMaksad")));
+            services.AddDbContext<AspenContext>(options => options.UseNpgsql(Configuration.GetConnectionString("local")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
