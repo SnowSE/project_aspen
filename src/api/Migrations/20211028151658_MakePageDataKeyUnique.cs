922c34c0c12df972619e440828e54a98abfbf202
Jonathan Allen <jallen@snow.edu>
2021-10-28 09:25:16 -0600

    Add a PageData/GetKeys endpoint; make PageData.Key unique in database

---
+++ src/api/Migrations/20211028151658_MakePageDataKeyUnique.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Api.Migrations
+{
+    public partial class MakePageDataKeyUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_PageData_Key",
+                table: "PageData",
+                column: "Key",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_PageData_Key",
+                table: "PageData");
+        }
+    }
+}
