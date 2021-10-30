61603ad7fa44c4e5e0be77fe64a35fd5536b1788
Emmanuel-Torres <emmanuel.torresamaya@gmail.com>
2021-10-30 17:53:21 -0600

    We are able to add and delete page data keys

--- src/web/src/components/PageData/PageDataList.js
+++ src/web/src/components/PageData/PageDataList.js
@@ -1,11 +1,32 @@
+import { useDispatch } from "react-redux";
+import { deletePageData, putPageData } from "../../store/pageDataSlice";
 import PageDataItem from "./PageDataItem";
 
 const PageDataList = (props) => {
-  console.log(props.keys);
+  const dispatch = useDispatch();
+
+  const editKeyHandler = (key) => {
+
+  };
+
+  const deleteKeyHandler = (key) => {
+    dispatch(deletePageData(key))
+  };
+
   return (
     <div>
       {props.keys.map((k) => {
-        return <PageDataItem pdKey={k} />;
+        return (
+          <div>
+            <p>{k ?? "Unnamed Key"}</p>
+            <button type="button" onClick={() => editKeyHandler(k)}>
+              Edit
+            </button>
+            <button type="button" onClick={() => deleteKeyHandler(k)}>
+              Delete
+            </button>
+          </div>
+        );
       })}
     </div>
   );
