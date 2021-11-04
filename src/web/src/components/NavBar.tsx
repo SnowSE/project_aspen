c4af503197f51742e7867567662c540f705eba1b
dallynowens2 <dallynowens2@gmail.com>
2021-11-04 21:41:57 +0000

    changes to navBar as well added a temp logo

--- src/web/src/components/NavBar.tsx
+++ src/web/src/components/NavBar.tsx
@@ -1,8 +1,10 @@
 import { AuthService } from "../services/authService";
 import { NavLink } from "react-router-dom";
 import { useStoreSelector } from "../store";
+import logo from "../tempLogo.png";
 
 const NavBar = () => {
+  const isAdmin = useStoreSelector((state) => state.auth.isAdmin);
   const isLoggedIn = useStoreSelector((state) => state.auth.isLoggedIn);
 
   const logoutHandler = () => {
@@ -12,17 +14,48 @@ const NavBar = () => {
     AuthService.signinRedirect();
   };
   return (
-    <nav className="navbar navbar-dark bg-secondary">
-      <div className="container-fluid justify-content-end">
-        <NavLink to ="/admin">Admin Page </NavLink>
-        <NavLink to="/pagedata">PageData</NavLink>
-        <NavLink to="/person">Person</NavLink>
+    <nav className="navbar navbar-dark bg-secondary row p-3 ">
+      <div className="container-fluid justify-content-start col ">
+        <img
+          src={logo}
+          className="img-fluid img-thumbnail rounded-circle m-2 p-2"
+          alt="logo"
+          width="100"
+          height="100"
+        />
+      </div>
+      <div className="container-fluid justify-content-end col">
+        {isAdmin ? (
+          <NavLink
+            className="border border-primary rounded p-2 bg-primary text-light m-1"
+            activeClassName="border border-primary rounded p-2 bg-light text-dark m-1"
+            to="/admin"
+          >
+            Admin Page
+          </NavLink>
+        ) : (
+          <div></div>
+        )}
+        <NavLink
+          className="border border-primary rounded p-2 bg-primary text-light m-1"
+          activeClassName="border border-primary rounded p-2 bg-light text-dark m-1"
+          to="/pagedata"
+        >
+          PageData
+        </NavLink>
+        <NavLink
+          className="border border-primary rounded p-2 bg-primary text-light m-1"
+          activeClassName="border border-primary rounded p-2 bg-light text-dark m-1"
+          to="/person"
+        >
+          Person
+        </NavLink>
         {isLoggedIn ? (
-          <button className="btn btn-primary" onClick={logoutHandler}>
+          <button className="btn btn-primary m-1" onClick={logoutHandler}>
             Logout
           </button>
         ) : (
-          <button className="btn btn-primary" onClick={loginHandler}>
+          <button className="btn btn-primary m-1" onClick={loginHandler}>
             Login
           </button>
         )}
