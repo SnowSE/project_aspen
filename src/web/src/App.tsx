df04c940971f243b89f5eb108cd1731b6c098271
Justin Holsclaw <jholsclaw@live.com>
2021-11-05 15:11:02 -0600

    added the authorized routes and admin routes.

--- src/web/src/App.tsx
+++ src/web/src/App.tsx
@@ -1,13 +1,39 @@
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 import { NavBar } from "./components/NavBar";
 import { Home } from "./views/Home";
 import { LoginLanding } from "./views/auth/LoginLanding";
 import { LogoutLanding } from "./views/auth/LogoutLanding";
 import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { FC, useEffect } from "react";
 import { checkIfLoggedIn } from "./store/authSlice";
+import { useStoreSelector } from "./store";
 import PageDataPage from "./views/PageDataPage";
 import Admin from "./views/Admin";
+
+const AuthorizedRoute: FC<any> = ({ children, authed: isAuthorized, ...rest }) => {
+    if (isAuthorized === true) {
+      return <Route {...rest}>{children}</Route>;
+    } else {
+      return (
+        <Redirect to={{ pathname: "/login"}} />
+      );
+    }
+  };
+
+const AdminRoute: FC<any> = ({ children, isAdmin, ...rest }) => {
+  if (isAdmin === true) {
+    return <Route {...rest}>{children}</Route>;
+  } else {
+    return (
+      <Redirect to={{ pathname: "/login"}} />
+    );
+  }
+};
 
 function App() {
   const dispatch = useDispatch();
@@ -15,24 +41,27 @@ function App() {
     dispatch(checkIfLoggedIn());
   }, [dispatch]);
 
+  const isAuthenticated = useStoreSelector((state) => state.auth.isLoggedIn);
+  const isAdmin = useStoreSelector((state) => state.auth.isAdmin);
+
   return (
     <Router>
       <NavBar />
       <Switch>
+        <AdminRoute authed={isAuthenticated} path="/admin">
+          <Admin />
+        </AdminRoute>
+        <AdminRoute authed={isAuthenticated} path="/pagedata">
+          <Admin />
+        </AdminRoute>
+        <AuthorizedRoute admin={isAdmin} path="/login/silent">
+          <LoginLanding/>
+        </AuthorizedRoute>
+        <AuthorizedRoute admin={isAdmin} path="/login/post">
+          <LoginLanding/>
+        </AuthorizedRoute>
         <Route path="/login/landing">
           <LoginLanding />
-        </Route>
-        <Route path="/login/silent">
-          <LoginLanding />
-        </Route>
-        <Route path="/logout/post">
-          <LogoutLanding />
-        </Route>
-        <Route path="/pagedata">
-          <PageDataPage />
-        </Route>
-        <Route path="/admin">
-          <Admin />
         </Route>
         <Route path="/">
           <Home />
