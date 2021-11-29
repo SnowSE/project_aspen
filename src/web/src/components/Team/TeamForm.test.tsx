a8c2b47ef79460435e58d5d74f9ed4010061fada
dallynowens2 <dallynowens2@gmail.com>
2021-11-29 13:47:24 -0700

    changed spacing

--- src/web/src/components/Team/TeamForm.test.tsx
+++ src/web/src/components/Team/TeamForm.test.tsx
@@ -3,31 +3,23 @@ import { BrowserRouter as Router } from "react-router-dom";
 import configureStore from "redux-mock-store";
 import TeamForm from "./TeamForm";
 import { Provider } from "react-redux";
-import thunk from 'redux-thunk'
+import thunk from "redux-thunk";
 
 describe("Team Form tests", () => {
-    const middlewares = [thunk]
-    function getEventList() {
-        return {
-          type: 'FETCH_DATA_SUCCESS'
-        }
-      }
+  const middlewares = [thunk];
   const initialState = {
-    event: 
+    event: {
+      events: [
         {
-            events:[
-                {
-                    name: "temp",
-                    date: new Date(2022, 6, 10)
-                },
-                {
-                    name: "temp2",
-                    date: new Date(2023, 6, 11)
-                }
-                
-            ] 
-            
-        }
+          name: "temp",
+          date: new Date(2022, 6, 10),
+        },
+        {
+          name: "temp2",
+          date: new Date(2023, 6, 11),
+        },
+      ],
+    },
   };
   const mockStore = configureStore(middlewares);
   let store;
