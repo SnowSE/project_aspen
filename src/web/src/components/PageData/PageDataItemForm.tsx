3f794584214c83a88b25a634534306b8f643cb3d
Rusty Hermansen <dr.delight@gmail.com>
2021-11-30 03:54:11 -0700

    fixed pagedata save/submit button

--- src/web/src/components/PageData/PageDataItemForm.tsx
+++ src/web/src/components/PageData/PageDataItemForm.tsx
@@ -1,5 +1,9 @@
-import { FormEvent, useState } from "react";
+import React, { FormEvent, useState, useEffect } from "react";
 import { PageData } from "../../models/pageData";
+import { useDispatch } from 'react-redux'
+import { alertActions } from "../../store/alertSlice";
+
+
 
 interface Props {
   pageData: PageData;
@@ -7,8 +11,14 @@ interface Props {
   onCancel: () => void;
 }
 const PageDataItemForm = ({ pageData, onCancel, onSubmit }: Props) => {
+  
   const [data, setData] = useState(JSON.stringify(pageData.data) ?? "");
   const [isDataValid, setIsDataValid] = useState(true);
+  const dispatch= useDispatch();
+  useEffect(() => {
+    
+    setData(JSON.stringify(pageData.data) ?? "")
+  }, [pageData])
 
   console.log(pageData.key, data, isDataValid);
 
@@ -28,12 +38,15 @@ const PageDataItemForm = ({ pageData, onCancel, onSubmit }: Props) => {
       onSubmit({ key: pageData.key, data: JSON.parse(data.toString()) });
     }
   };
+  const displayAlert = () => 
+    dispatch(alertActions.displayAlert({title: "Success!", message: "Successfully Submitted!", danger: false}));
 
   return (
     <div className="border p-3">
       <form onSubmit={submitHandler}>
         <label className="form-label fs-5">{pageData.key}</label>
-        <textarea className="form-control" value={data.toString()} onChange={dataChangedHandler} />
+        {/* <TextInput ></TextInput> */}
+        <textarea className="form-control" value={data} onChange={dataChangedHandler} />
         <button type="submit" className="btn btn-primary btn-sm me-1 my-2">Save</button>
         <button type="button" className="btn btn-secondary btn-sm ms-1 my-2" onClick={() => onCancel()}>
           Cancel
