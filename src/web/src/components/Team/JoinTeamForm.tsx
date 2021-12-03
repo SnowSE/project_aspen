0fdd63c51087615a761346b601c9d60d92230640
Emmanuel Torres <emmanuel.torresamaya@gmail.com>
2021-12-03 12:58:48 -0700

    added notifications for registrations

--- src/web/src/components/Team/JoinTeamForm.tsx
+++ src/web/src/components/Team/JoinTeamForm.tsx
@@ -1,10 +1,12 @@
 import { FC, FormEvent, useState } from "react";
-import { useDispatch } from "react-redux";
+import { StoreDispatch } from "../../store";
 import useInput from "../../hooks/use-input";
 import TextInput from "../../inputs/TextInput";
 import Registration from "../../models/registration";
 import Team from "../../models/team";
 import { createRegistration } from "../../store/teamSlice";
+import { useDispatch } from "react-redux";
+import { alertActions } from "../../store/alertSlice";
 
 type Props = {
     team: Team;
@@ -14,7 +16,7 @@ type Props = {
 
 const JoinTeamForm: FC<Props> = (props): JSX.Element => {
     const [isPublic, setIsPublic] = useState(false);
-    const dispatch = useDispatch();
+    const dispatch = useDispatch<StoreDispatch>();
 
     const nickname = useInput(
         "Registration Nickname",
@@ -33,7 +35,15 @@ const JoinTeamForm: FC<Props> = (props): JSX.Element => {
                 props.team.id,
             )
 
-            dispatch(createRegistration(registration));
+            dispatch(createRegistration(registration))
+                .then(r => {
+                    if (r.meta.requestStatus === 'fulfilled') {
+                        dispatch(alertActions.displayAlert({ title: 'Registration Succcessful', message: 'Your registration has been processed' }))
+                    }
+                    else {
+                        dispatch(alertActions.displayAlert({ title: 'Registration Failed', message: `Your registration has been rejected.`, danger: true }))
+                    }
+                })
         }
     }
 
