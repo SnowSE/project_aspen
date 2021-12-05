608e8726684ed28888834816be26248829e6e5ba
Rusty Hermansen <dr.delight@gmail.com>
2021-12-05 00:15:04 -0700

    got eventTeams.test.tsx to pass

--- src/web/src/components/Events/EventTeams.test.tsx
+++ src/web/src/components/Events/EventTeams.test.tsx
@@ -2,14 +2,29 @@ import { render, screen } from "@testing-library/react";
 import { BrowserRouter as Router} from "react-router-dom";
 import EventModel from "../../models/eventModel";
 import EventTeams from "./EventTeams";
+import thunk from 'redux-thunk'
+import configureStore from 'redux-mock-store'
+import { Provider } from "react-redux";
 
 describe("Event Teams tests", ()=>{
+    const middlewares = [thunk];
+    const initialState = {
 
+        team: {
+            teamList: []
+        }
+    }
+    const mockStore = configureStore(middlewares);
+    const store = mockStore(initialState)
     test('Checks for Registered Teams text', ()=>{
+    
         render(
-            <Router>
+            <Provider store={store}>
+                <Router>
                 <EventTeams event={new EventModel(undefined,undefined,undefined,undefined,undefined)}/>
             </Router>
+            </Provider>
+            
         );
 
         const formElement = screen.getByText(/Registered Teams/)
@@ -18,9 +33,13 @@ describe("Event Teams tests", ()=>{
 
     test('Checks for joint team button', ()=>{
         render(
-            <Router>
+            
+            <Provider store={store}>
+                 <Router>
                 <EventTeams event={new EventModel(undefined,undefined,undefined,undefined,undefined)}/>
             </Router>
+            </Provider>
+           
         );
 
         const formElement = screen.getByText(/Join Team/);
