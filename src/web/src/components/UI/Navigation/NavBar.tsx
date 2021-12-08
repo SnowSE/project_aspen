560c9a55f99d316da72f3442b5203f7003d5a938
Hecedu <hector.maganahdez@gmail.com>
2021-12-07 17:57:38 -0700

    Benign code removal navbar changes

--- src/web/src/components/UI/Navigation/NavBar.tsx
+++ src/web/src/components/UI/Navigation/NavBar.tsx
@@ -14,43 +14,34 @@ const NavBar = () => {
     AuthService.signinRedirect();
   };
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-secondary">
-      <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">
-          <img
-            src={logo}
-            className="img-fluid img-thumbnail rounded-circle m-2 p-2"
-            alt="logo"
-            width="50"
-            height="50"
-          />
-        </NavLink>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav justify-content-end container-fluid">
-            <li className="nav-item mx-2 p-1 text-center text-light">
-              {isLoggedIn ? (
-                <div className="d-flex align-items-center">
-                  <p className="text-black m-0 me-3">Welcome back, {userName}!</p>
-                  <button className="btn btn-primary me-2" onClick={logoutHandler}>
-                    Logout
-                  </button>
-                </div>
-              ) : (
-                <button className="btn btn-primary me-2" onClick={loginHandler}>
-                  Login
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary py-3">
+      <NavLink className="navbar-brand" to="/">Project Aspen</NavLink>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarSupportedContent"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav justify-content-end container-fluid">
+          <li className="nav-item text-center text-light">
+            {isLoggedIn ? (
+              <div className="d-flex align-items-center">
+                <p className="m-0 me-3">Welcome back, {userName}!</p>
+                <button className="btn btn-primary " onClick={logoutHandler}>
+                  Logout
                 </button>
-              )}
-            </li>
-          </ul>
-        </div>
+              </div>
+            ) : (
+              <button className="btn btn-primary" onClick={loginHandler}>
+                Login
+              </button>
+            )}
+          </li>
+        </ul>
       </div>
     </nav>
   );
