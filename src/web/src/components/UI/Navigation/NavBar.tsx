1742824d64fa261464f8d27ab2144d4f80175bab
Hecedu <hector.maganahdez@gmail.com>
2021-12-13 01:08:41 -0700

    favicon

--- src/web/src/components/UI/Navigation/NavBar.tsx
+++ src/web/src/components/UI/Navigation/NavBar.tsx
@@ -13,8 +13,8 @@ const NavBar = () => {
     AuthService.signinRedirect();
   };
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-primary px-5 py-3">
-      <NavLink className="navbar-brand" to="/">Project Aspen</NavLink>
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary px-3 py-3">
+      <NavLink className="navbar-brand" to="/">PROJECT ASPEN</NavLink>
       <button
         className="navbar-toggler"
         type="button"
@@ -25,17 +25,17 @@ const NavBar = () => {
       </button>
       
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav justify-content-end container-fluid">
+        <ul className="navbar-nav justify-content-end container-fluid p-0">
           <li className="nav-item text-center text-light">
             {isLoggedIn ? (
               <div className="d-flex align-items-center">
                 <p className="m-0 me-3">Welcome back, {userName}!</p>
-                <button className="btn btn-primary " onClick={logoutHandler}>
+                <button className="btn btn-primary m-0 " onClick={logoutHandler}>
                   Logout
                 </button>
               </div>
             ) : (
-              <button className="btn btn-primary" onClick={loginHandler}>
+              <button className="btn btn-primary m-0" onClick={loginHandler}>
                 Login
               </button>
             )}
