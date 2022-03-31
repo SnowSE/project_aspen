76d1d79c1c76d04a374d70b255d3b9e022c804ed
Jonathan Allen <jallen@snow.edu>
2022-03-31 11:10:53 -0600

    remove storage account name, it will ask

--- ops/terraform/aspen.tf
+++ ops/terraform/aspen.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "azurerm" {
     resource_group_name = "terraform-resources"
-    storage_account_name = "jallenaspenstorage"
     container_name = "terraform"
     key = "aspen.tfstate"
   }
