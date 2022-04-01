66f22cfc7c60cb4ca641cba9bee1b741018ef80a
Jonathan Allen <jallen@snow.edu>
2022-04-01 17:06:07 -0600

    dump configuration

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -13,6 +13,11 @@ public class Program
         {
             var db = scope.ServiceProvider.GetRequiredService<AspenContext>();
             var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+            var config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+            foreach (var configItem in config.AsEnumerable())
+            {
+                logger.LogInformation($"{configItem.Key} {configItem.Value}");
+            }
             logger.LogInformation("*#*#*#*  Connection String: " + db.Database.GetConnectionString());
             db.Database.Migrate();  //Whenever I try to run this against the already existing db, it gives error, but we need this line for testing
         }
