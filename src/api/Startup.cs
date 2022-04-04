5a73f07d74c7dc205b4c27b2318b4e4f0ec87fd1
Jonathan Allen <jallen@snow.edu>
2022-04-04 10:03:15 -0600

    update smoke test v6

--- src/api/Startup.cs
+++ src/api/Startup.cs
@@ -160,7 +160,7 @@ public class Startup
 
         app.UseEndpoints(endpoints =>
         {
-            endpoints.MapGet("/health", c => c.Response.WriteAsync("ur good (v5)"));
+            endpoints.MapGet("/health", c => c.Response.WriteAsync("ur good (v6)"));
             endpoints.MapControllers();
         });
     }
