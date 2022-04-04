93e2f24292b8c6d672645c077915c91bbbf91314
Esplintk <59877903+Esplintk@users.noreply.github.com>
2022-04-04 15:39:49 -0600

    Fixed the Azure-Trent-yml file

--- ops/terraform/aspen.tf
+++ ops/terraform/aspen.tf
@@ -54,7 +54,7 @@ resource "azurerm_postgresql_firewall_rule" "api_db_access" {
 }
 
 ##############################################################################
-# App Services
+# App Services  
 ##############################################################################
 
 resource "azurerm_service_plan" "main" {
