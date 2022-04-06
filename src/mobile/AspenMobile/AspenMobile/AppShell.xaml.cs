752ecece4cec0e95ecc5708f0ade4a2387ebd416
Jonathan Allen <jallen@snow.edu>
2022-04-06 15:29:47 -0600

    add mobile project

---
+++ src/mobile/AspenMobile/AspenMobile/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using AspenMobile.ViewModels;
+using AspenMobile.Views;
+using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace AspenMobile
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
