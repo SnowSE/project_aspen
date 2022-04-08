006b7facaa3850f172bec5dc9a9de51f7e162e65
jayse.baruch <jayse.baruch@ENGR2UA8041PG1>
2022-04-08 14:53:43 -0600

    beginning of donation details for admins

--- src/mobile/AspenMobile/AspenMobile/ViewModels/AboutViewModel.cs
+++ src/mobile/AspenMobile/AspenMobile/ViewModels/AboutViewModel.cs
@@ -1,18 +1,66 @@
-﻿using System;
+﻿using IdentityModel.OidcClient;
+using IdentityModel.OidcClient.Browser;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace AspenMobile.ViewModels
 {
-    public class AboutViewModel : BaseViewModel
+    public class AboutViewModel : ObservableObject
     {
         public AboutViewModel()
         {
-            Title = "About";
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            
+            var browser = DependencyService.Get<IBrowser>();
+            var options = new OidcClientOptions
+            {
+                Authority = "https://engineering.snow.edu/aspen/auth/realms/aspen",
+                ClientId = "aspen-web",
+                Scope = "profile email api-use",
+                RedirectUri = "xamarinformsclients://callback",
+                Browser = browser
+            };
+            client = new OidcClient(options);
+            apiClient.Value.BaseAddress = new Uri("https://engineering.snow.edu/aspen/auth/realms/aspen");
+            OutputText = "Let's a Go";
         }
 
         public ICommand OpenWebCommand { get; }
+        private OidcClient client;
+        private LoginResult result;
+        private Lazy<HttpClient> apiClient = new Lazy<HttpClient>(() => new HttpClient());
+        private string accessToken;
+
+
+        [ObservableProperty]
+        private string outputText;
+
+        [ICommand]
+        private async Task CallEvents()
+        {
+            try
+            {
+                var currentEventId = 0;
+                var result = await apiClient.Value.GetAsync("api/admin/donations/{currentEventId}");
+
+                if (result.IsSuccessStatusCode)
+                {
+                    OutputText = JsonDocument.Parse(await result.Content.ReadAsStringAsync()).RootElement.GetRawText();
+                }
+                else
+                {
+                    OutputText = result.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+                OutputText = ex.ToString();
+            }
+        }
     }
 }
--- src/mobile/AspenMobile/AspenMobile/Views/AboutPage.xaml.cs
+++ src/mobile/AspenMobile/AspenMobile/Views/AboutPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AspenMobile.ViewModels;
+using System;
 using System.ComponentModel;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -10,6 +11,7 @@ namespace AspenMobile.Views
         public AboutPage()
         {
             InitializeComponent();
+            BindingContext = new AboutViewModel();
         }
     }
 }
