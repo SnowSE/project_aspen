37f85c3d3c39ffa4e13bda548aa197a8f65388f1
sergio-ayala123 <venturian6@gmail.com>
2022-04-09 23:32:30 -0600

    AllEventsPage and EventPage Progress

--- src/mobile/AspenMobile/AspenMobile/ViewModels/AboutViewModel.cs
+++ src/mobile/AspenMobile/AspenMobile/ViewModels/AboutViewModel.cs
@@ -2,15 +2,60 @@
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using AspenMobile.Models;
+using AspenMobile.GlobalConstants;
+using AspenMobile.Views;
+using Microsoft.Toolkit.Mvvm.Input;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Net.Http.Json;
+using System.Collections.Generic;
+using shared.DtoModels;
+using System.Collections.ObjectModel;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
 
 namespace AspenMobile.ViewModels
 {
-    public class AboutViewModel 
+    public partial class AboutViewModel : ObservableObject
     {
+        private readonly HttpClient httpClient = new();
         public AboutViewModel()
         {
-            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+            var current = Preferences.Get(Constants.CurrentServer, null);
+            if (current == null)
+            {
+                Shell.Current.GoToAsync($"{nameof(SettingsPage)}");
+            }
+            Test = "chef";
         }
-        public ICommand OpenWebCommand { get; }
+
+        public ObservableCollection<DtoEvent> Events { get; set; } = new();
+
+
+        [ObservableProperty]
+        public string test;
+
+        [ICommand]
+        public async Task GetAllEvents()
+        {
+            var allEvents = await httpClient.GetFromJsonAsync<List<DtoEvent>>("https://engineering.snow.edu/aspen/api/events");
+
+            foreach (var item in allEvents)
+            {
+                Events.Add(item);
+            }
+        }
+
+        [ICommand]
+        public async void SelectEvent(DtoEvent item)
+        {
+            if (item == null)
+                return;
+
+            // This will push the ItemDetailPage onto the navigation stack
+            //await Shell.Current.GoToAsync($"{nameof(EventPage)}?{nameof(EventPageViewModel.ItemId)}={item.ID}");
+            await Shell.Current.GoToAsync("/EventPage");
+
+        }
     }
 }
---
+++ src/mobile/AspenMobile/AspenMobile/ViewModels/EventPageViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
+using shared.DtoModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AspenMobile.ViewModels
+{
+    [QueryProperty(nameof(ItemId), nameof(ItemId))]
+
+    public partial class EventPageViewModel : ObservableObject
+    {
+        private readonly HttpClient httpClient;
+        public EventPageViewModel()
+        {
+
+        }
+        /*[ObservableProperty]
+        public string itemId;*/
+
+        public string ItemId { get; set; }
+        public ObservableCollection<DtoEvent> Event { get; set; } = new();
+        public ObservableCollection<DtoTeam> Teams { get; set; } = new();
+
+
+
+        [ICommand]
+        public async Task DisplayEventAsync()
+        {
+            var currentEvent = await httpClient.GetFromJsonAsync<DtoEvent>($"https://engineering.snow.edu/aspen/api/events/{ItemId}");
+            Event.Add(currentEvent);
+
+            var teams = await httpClient.GetFromJsonAsync<List<DtoTeam>>($"https://engineering.snow.edu/aspen/api/teams/event/{currentEvent.ID}");
+
+            foreach(var team in teams)
+            {
+                Teams.Add(team);
+            }
+        }
+    }
+}
