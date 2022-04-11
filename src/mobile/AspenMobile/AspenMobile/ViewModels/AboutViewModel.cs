1957cef902658b206724c97b24dd1478a3295976
jayse.baruch <jayse.baruch@ENGR2UA8041PG1>
2022-04-11 15:52:52 -0600

    working on donation details

--- src/mobile/AspenMobile/AspenMobile/ViewModels/AboutViewModel.cs
+++ src/mobile/AspenMobile/AspenMobile/ViewModels/AboutViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using IdentityModel.OidcClient;
 using IdentityModel.OidcClient.Browser;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -11,11 +14,12 @@ using Xamarin.Forms;
 
 namespace AspenMobile.ViewModels
 {
-    public class AboutViewModel : ObservableObject
+    public partial class AboutViewModel : ObservableObject
     {
         public AboutViewModel()
         {
-            
+            OpenWebCommand = new Command(async () => await Browser.OpenAsync("https://aka.ms/xamarin-quickstart"));
+
             var browser = DependencyService.Get<IBrowser>();
             var options = new OidcClientOptions
             {
@@ -25,28 +29,27 @@ namespace AspenMobile.ViewModels
                 RedirectUri = "xamarinformsclients://callback",
                 Browser = browser
             };
-            client = new OidcClient(options);
-            apiClient.Value.BaseAddress = new Uri("https://engineering.snow.edu/aspen/auth/realms/aspen");
+            _client = new OidcClient(options);
+            _apiClient.Value.BaseAddress = new Uri("https://engineering.snow.edu/aspen/auth/realms/aspen");
             OutputText = "Let's a Go";
         }
 
-        public ICommand OpenWebCommand { get; }
-        private OidcClient client;
-        private LoginResult result;
-        private Lazy<HttpClient> apiClient = new Lazy<HttpClient>(() => new HttpClient());
-        private string accessToken;
 
+        public ICommand OpenWebCommand { get; }
+        public OidcClient _client;
+        public LoginResult _result;
+        public Lazy<HttpClient> _apiClient = new Lazy<HttpClient>(() => new HttpClient());
+        public string accessToken;
 
         [ObservableProperty]
         private string outputText;
 
         [ICommand]
-        private async Task CallEvents()
+        private async Task GetDonationInfo()
         {
             try
             {
-                var currentEventId = 0;
-                var result = await apiClient.Value.GetAsync("api/admin/donations/{currentEventId}");
+                var result = await _apiClient.Value.GetAsync("api/Admin/donation/{eventID}");
 
                 if (result.IsSuccessStatusCode)
                 {
@@ -63,4 +66,5 @@ namespace AspenMobile.ViewModels
             }
         }
     }
+
 }
