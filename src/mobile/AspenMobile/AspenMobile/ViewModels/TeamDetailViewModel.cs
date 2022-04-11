e9713565662c599ce206fc7c5d6a5db69a5fd734
hojayevadurli <59879064+hojayevadurli@users.noreply.github.com>
2022-04-11 12:50:49 -0600

    added teamsPage

---
+++ src/mobile/AspenMobile/AspenMobile/ViewModels/TeamDetailViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AspenMobile.ViewModels
+{
+    public class TeamDetailViewModel : ObservableObject
+    {
+        public TeamDetailViewModel()
+        {
+           
+        }
+    }
+}
