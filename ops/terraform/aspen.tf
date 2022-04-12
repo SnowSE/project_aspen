5b2ed0f0e9841acb6151da06d5ef7cbd2cc9b6e4
Maksad Annamuradov <maksad.annamuradow98@gmail.com>
2022-04-11 22:22:42 -0600

    modified aspen.tf to create azure vm

--- ops/terraform/aspen.tf
+++ ops/terraform/aspen.tf
@@ -57,47 +57,146 @@ resource "azurerm_postgresql_firewall_rule" "api_db_access" {
 # ElasticSearch and Kibana
 ##############################################################################
 
+# Create virtual network
+resource "azurerm_virtual_network" "myterraformnetwork" {
+  name                = "myVnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.aspenrg.location
+  resource_group_name = azurerm_resource_group.aspenrg.name
+}
+
+# Create subnet
+resource "azurerm_subnet" "myterraformsubnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.aspenrg.name
+  virtual_network_name = azurerm_virtual_network.myterraformnetwork.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Create public IPs
+resource "azurerm_public_ip" "myterraformpublicip" {
+  name                = "myPublicIP"
+  location            = azurerm_resource_group.aspenrg.location
+  resource_group_name = azurerm_resource_group.aspenrg.name
+  allocation_method   = "Dynamic"
+}
+
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "myterraformnsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = azurerm_resource_group.aspenrg.location
+  resource_group_name = azurerm_resource_group.aspenrg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+   security_rule {
+    name                       = "ElasticSearch"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9200"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+   security_rule {
+    name                       = "Kibana"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5601"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Create network interface
+resource "azurerm_network_interface" "myterraformnic" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.aspenrg.location
+  resource_group_name = azurerm_resource_group.aspenrg.name
+
+  ip_configuration {
+    name                          = "myNicConfiguration"
+    subnet_id                     = azurerm_subnet.myterraformsubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.myterraformpublicip.id
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.myterraformnic.id
+  network_security_group_id = azurerm_network_security_group.myterraformnsg.id
+}
+
+# Create storage account for boot diagnostics
+resource "azurerm_storage_account" "mystorageaccount" {
+  name                     = "diag${random_id.randomId.hex}"
+  location                 = azurerm_resource_group.aspenrg.location
+  resource_group_name      = azurerm_resource_group.aspenrg.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
+
+
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "myterraformvm" {
-    name                  = "myVM"
+    name                  = "Aspen-Telemetry${random_id.randomId.hex}"
     location              = azurerm_resource_group.aspenrg.location
     resource_group_name   = azurerm_resource_group.aspenrg.name
     network_interface_ids = [azurerm_network_interface.myterraformnic.id]
-    size                  = "Standard_DS1_v2"
+    size                  = "Standard_B2s"
 
     os_disk {
-      name                 = "myOsDisk"
+      name                 = "myOsDisk${random_id.randomId.hex}"
+      osType               ="Linux"
       caching              = "ReadWrite"
+      diskSizeGB           = 30
       storage_account_type = "Premium_LRS"
     }
 
     source_image_reference {
-      publisher = "Canonical"
-      offer     = "UbuntuServer"
-      sku       = "18.04-LTS"
-      version   = "latest"
+       publisher            = "canonical"
+        offer               = "0001-com-ubuntu-server-focal"
+        sku                 = "20_04-lts-gen2"
+        version             = "latest"
     }
 
-    computer_name                   = "myvm"
+    computer_name                   = "aspen-telemetry-vm"
     admin_username                  = "azureuser"
-    disable_password_authentication = true
-
-    admin_ssh_key {
-      username   = "azureuser"
-      public_key = tls_private_key.example_ssh.public_key_openssh
-    }
+    admin_password                  = "Password1234!"
+    disablePasswordAuthentication   = false
 
     boot_diagnostics {
       storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
     }
 
-  resource "azurerm_postgresql_firewall_rule" "api_db_access" {
-    name = "api_db_access-${random_id.id.hex}"
-    resource_group_name = azurerm_resource_group.aspenrg.name
-    server_name = azurerm_postgresql_server.api.name
-    start_ip_address = "0.0.0.0"
-    end_ip_address = "0.0.0.0"
-  }
+    # provisioner "file" {
+    #   source      = "staging/"
+    #   destination = "/app"
+    # }
+
+    # provisioner "remote-exec" {
+    #   inline = [
+    #     "cd /app",
+    #     "docker-compose up",
+    #   ]
+    # }
 }
 
 ##############################################################################
