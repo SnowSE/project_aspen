e01eabc3d14f660c38a3663449c58205949d6304
Jesse Madsen <madsencad@gmail.com>
2022-04-13 13:04:10 -0600

    Telemetry changes, Not a good Idea to merge with your branch（￣︶￣)

--- src/api/Controllers/AdminController.cs
+++ src/api/Controllers/AdminController.cs
@@ -11,12 +11,15 @@ public class AdminController : ControllerBase
     private readonly IDonationRepository donationRepository;
     private readonly IEventRepository eventRepository;
     private readonly IMapper mapper;
+    private readonly ILogger<AdminController> log;
+    private readonly ILogger<AdminController> logger;
 
-    public AdminController(IDonationRepository donationRepository, IEventRepository eventRepository, IMapper mapper)
+    public AdminController(IDonationRepository donationRepository, IEventRepository eventRepository, IMapper mapper, ILogger<AdminController> log)
     {
         this.donationRepository = donationRepository;
         this.eventRepository = eventRepository;
         this.mapper = mapper;
+        this.log = log;
     }
 
     [HttpGet, Authorize(Roles = AspenAdminRole)]
@@ -27,6 +30,7 @@ public class AdminController : ControllerBase
     public async Task<IEnumerable<DtoDonation>> GetEventDonations(long eventID)
     {
         var donations = await donationRepository.GetByEventIdAsync(eventID);
+        log.LogInformation("AdminController: GetEventDonations eventID: {id}", eventID);
         return mapper.Map<IEnumerable<Donation>, IEnumerable<DtoDonation>>(donations);
     }
 
@@ -34,6 +38,7 @@ public class AdminController : ControllerBase
     public async Task<IEnumerable<DtoDonation>> GetTeamDonations(long eventID, long teamID)
     {
         var donations = await donationRepository.GetByTeamIdAsync(eventID, teamID);
+        log.LogInformation("AdminController: GetTeamDonations eventID: {eventid}, teamID: {teamid}", eventID, teamID);
         return mapper.Map<IEnumerable<Donation>, IEnumerable<DtoDonation>>(donations);
     }
 }
--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Sinks.Elasticsearch;
 
 namespace Api;
 
@@ -8,14 +10,16 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var host = CreateHostBuilder(args)
-            .ConfigureLogging(logging =>
-            {
-                logging.ClearProviders();
-                logging.AddConsole();
-                logging.AddAzureWebAppDiagnostics();
-            })
-            .Build();
+       ConfigureLogging();
+       var host = CreateHostBuilder(args).Build();
+        //var host = CreateHostBuilder(args)
+        //    .ConfigureLogging(logging =>
+        //    {
+        //        logging.ClearProviders();
+        //        logging.AddConsole();
+        //        logging.AddAzureWebAppDiagnostics();
+        //    })
+        //    .Build();
         using (var scope = host.Services.CreateScope())
         {
             var db = scope.ServiceProvider.GetRequiredService<AspenContext>();
@@ -24,6 +28,26 @@ public class Program
         }
         host.Run();
     }
+
+    private static void ConfigureLogging()
+        {
+            var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile(
+                    $"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json",
+                    optional: true)
+                .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                .Enrich.FromLogContext()
+                .WriteTo.Debug()
+                .WriteTo.Console()
+                .WriteTo.Elasticsearch(ConfigureElasticSink(configuration, environment))
+                .Enrich.WithProperty("Environment", environment)
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+        }
 
     private static void dumpLogs(IServiceScope scope, AspenContext db)
     {
@@ -35,6 +59,14 @@ public class Program
             logger.LogInformation($"{configItem.Key} {configItem.Value}");
         }
     }
+            private static ElasticsearchSinkOptions ConfigureElasticSink(IConfigurationRoot configuration, string environment)
+        {
+            return new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+            {
+                AutoRegisterTemplate = true,
+                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}"
+            };
+        }
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
