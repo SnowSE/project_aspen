c873bcb962fc86c3ab75810665cab1a9a1f37bc7
Durli Hojayeva <hojayevadurli@gmail.com>
2022-04-12 23:13:31 -0600

    added TeamDetailViewModel

---
+++ src/mobile/AspenMobile/AspenMobile/ViewModels/TeamDetailViewModel.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Toolkit.Mvvm.ComponentModel;
+using shared.DtoModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace AspenMobile.ViewModels
+{
+    public partial class TeamDetailViewModel : ObservableObject
+    {
+        public ObservableCollection<DtoTeam> TeamInfoList { get; }
+
+        public TeamDetailViewModel()
+        {
+           
+        }
+
+        [ObservableProperty]
+        private string teamName;
+        [ObservableProperty]
+        private long teamId;
+        [ObservableProperty]
+        private string teamDescription;
+        [ObservableProperty]
+        private string mainImage;
+        [ObservableProperty]
+        private long ownerID;
+        [ObservableProperty]
+        private long eventID;
+        [ObservableProperty]
+        private decimal donationTarget;
+
+
+    }
+}
