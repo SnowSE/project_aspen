3c59893fb701acf10e79d37f87e7a7136dfd0426
Jonathan Allen <jallen@snow.edu>
2022-04-13 15:27:56 -0600

    Call team api instead of web page for teams

--- src/mobile/AspenMobile/AspenMobile/ViewModels/TeamDetailViewModel.cs
+++ src/mobile/AspenMobile/AspenMobile/ViewModels/TeamDetailViewModel.cs
@@ -1,17 +1,13 @@
 ﻿using AspenMobile.GlobalConstants;
 using Microsoft.Toolkit.Mvvm.ComponentModel;
-using Microsoft.Toolkit.Mvvm.Input;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using shared.DtoModels;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 using System.Net.Http;
-using System.Text;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
 using Xamarin.Essentials;
-using Xamarin.Forms;
 
 namespace AspenMobile.ViewModels
 {
@@ -24,75 +20,30 @@ namespace AspenMobile.ViewModels
             GetTeamInfoAsync();
         }
 
-        //[ObservableProperty]
-        //private string teamName;
-        //[ObservableProperty]
-        //private long teamId;
-        //[ObservableProperty]
-        //private string teamDescription;
-        //[ObservableProperty]
-        //private string mainImage;
-        //[ObservableProperty]
-        //private long ownerID;
-        //[ObservableProperty]
-        //private long eventID;
-        //[ObservableProperty]
-        //private decimal donationTarget;
+        [ObservableProperty]
+        private string errorMessage;
 
-       
-        public async void GetTeamInfoAsync()
+        private int teamId;//needs to be set by naviagation parameter
+
+        public async Task GetTeamInfoAsync()
         {
-            var uri = new Uri("https://engineering.snow.edu/aspen/team/36");
-            // var TeamInfo = Preferences.Get("TheTeam", null);
             var httpClient = new HttpClient();
 
             //Need to ask a Jonathan which string to use
-            var response = await httpClient.GetAsync(uri);
-
-            if(response.IsSuccessStatusCode)
+            try
             {
-                var content = await response.Content.ReadAsStringAsync();
-                string json = content.ToString();
-                var dtoTeamInfos = JsonConvert.DeserializeObject<List<DtoTeam>>(json);
-               var jsonList = JArray.Parse(dtoTeamInfos.ToString());
-                foreach(var dtoTeamInfo in dtoTeamInfos)
+                var server = Preferences.Get(Constants.CurrentServer, null) ?? throw new Exception("No server address set");
+                var uri = new Uri($"{server}/api/teams/{teamId}");
+                var teams = await httpClient.GetFromJsonAsync<List<DtoTeam>>(uri);
+                foreach (var team in teams)
                 {
-                    DtoTeam team = new DtoTeam();
-                    string teamName = dtoTeamInfo.Name.ToString();
-                    long teamId = dtoTeamInfo.ID;
-                    string teamDescription=dtoTeamInfo.Description;
-                    long ownerID=dtoTeamInfo.OwnerID;
-                    long eventID=dtoTeamInfo.EventID;
-                    decimal donationTarget=dtoTeamInfo.DonationTarget;
-
-                    team.Name = teamName;
-                    team.Description = teamDescription;
-                    team.OwnerID = ownerID;
-                    team.EventID = eventID;
-                    team.DonationTarget = donationTarget;
-                    team.ID = teamId;
-
                     TeamInfoList.Add(team);
                 }
             }
-             
-            
-            // assign the values 
-            //team.TeamName = teamName;
-            //team.TeamId = teamId;
-            //team.OwnerID = ownerID;
-            //team.EventID = eventID;
-            //team.DonationTarget = donationTarget;
-            ////team.TeamDescription = teamDescription;
-            //team.MainImage = mainImage;
-           
-
-            //var theTeam = Preferences.Get(Constants.TeamInfo, TeamInfoList);
-
-
-
-
-
+            catch (Exception ex)
+            {
+                ErrorMessage = ex.Message;
+            }
         }
 
 
