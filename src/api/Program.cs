847a3762c9c11311a7e02535997c33f0981b8271
MaksadAnnamuradov <maksad.annamuradow98@gmail.com>
2022-04-15 10:34:45 -0600

    changed to private ip

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -51,7 +51,7 @@ public class Program
             .Enrich.FromLogContext()
             .WriteTo.Console()
             .WriteTo.Debug()
-            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://104.43.240.156:9200/"))
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://10.1.0.4:9200/"))
             {
                 AutoRegisterTemplate = true,
                 AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
