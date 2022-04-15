ab9ed7a27946ea0781d26b2c11b8a63c1c6ee361
hojayevadurli <59879064+hojayevadurli@users.noreply.github.com>
2022-04-15 01:39:59 -0600

    added sinks and emitters to program.cs

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 using Serilog.Sinks.Elasticsearch;
 using System.Reflection;
 
@@ -10,20 +11,20 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        //var configuration = new ConfigurationBuilder()
-        //.AddJsonFile("appsettings.json")
-        //.Build();
-        //Log.Logger = (Serilog.ILogger)new LoggerConfiguration()
-        //.WriteTo.Console()
-        //.WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
-        //{
-        //    AutoRegisterTemplate = true,
-        //    IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{DateTime.UtcNow:yyyy-MM}"
-        //})
-        //.WriteTo.File("Logs/LogInfo.txt")
-        //.WriteTo.Seq("http://localhost:5341",
-        //apiKey: "yeEZyL3SMcxEKUijBjN")
-        //.CreateLogger();
+        var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json")
+        .Build();
+         Log.Logger = (Serilog.ILogger)new LoggerConfiguration()
+        .WriteTo.Console()
+        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
+        {
+            AutoRegisterTemplate = true,
+            IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{DateTime.UtcNow:yyyy-MM}"
+        })
+        .WriteTo.File("Logs/LogInfo.txt")
+        .WriteTo.Seq("http://localhost:5341",
+        apiKey: "yeEZyL3SMcxEKUijBjN")
+        .CreateLogger();
         var host = CreateHostBuilder(args)
            .ConfigureLogging(logging =>
            {
@@ -47,5 +48,6 @@ public class Program
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
-            });
+            })
+         .UseSerilog();
 }
