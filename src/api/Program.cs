47585491ed420c58b774997ba7c1ea5a236345d5
sergio-ayala123 <venturian6@gmail.com>
2022-04-17 20:51:58 -0600

    serilog stuff

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -47,6 +47,7 @@ public class Program
 
     public static IHostBuilder CreateHostBuilder(string[] args) =>
         Host.CreateDefaultBuilder(args)
+            .UseSerilog()
             .ConfigureWebHostDefaults(webBuilder =>
             {
                 webBuilder.UseStartup<Startup>();
