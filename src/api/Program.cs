c52758c3a99185282a8083e518b7cbedfbed96ee
hojayevadurli <59879064+hojayevadurli@users.noreply.github.com>
2022-04-18 00:05:39 -0600

    added logging

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Serilog;
+using Serilog.Events;
 using Serilog.Sinks.Elasticsearch;
 using System.Reflection;
 
@@ -11,28 +12,17 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var configuration = new ConfigurationBuilder()
-        .AddJsonFile("appsettings.json")
-        .Build();
-         Log.Logger = (Serilog.ILogger)new LoggerConfiguration()
-        .WriteTo.Console()
-        .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(configuration["ElasticConfiguration:Uri"]))
-        {
-            AutoRegisterTemplate = true,
-            IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name.ToLower()}-{DateTime.UtcNow:yyyy-MM}"
-        })
-        .WriteTo.File("Logs/LogInfo.txt")
-        .WriteTo.Seq("http://localhost:5341",
-        apiKey: "yeEZyL3SMcxEKUijBjN")
-        .CreateLogger();
+       
+
+
         var host = CreateHostBuilder(args)
-           .ConfigureLogging(logging =>
-           {
-               logging.ClearProviders();
-               logging.AddConsole();
-               logging.AddAzureWebAppDiagnostics();
-           })
-           .Build();
+            .ConfigureLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging.AddConsole();
+                logging.AddAzureWebAppDiagnostics();
+            })
+            .Build();
         using (var scope = host.Services.CreateScope())
         {
             var db = scope.ServiceProvider.GetRequiredService<AspenContext>();
@@ -40,6 +30,22 @@ public class Program
             db.Database.Migrate();
         }
         host.Run();
+    }
+    private static void ConfigureLogging(string environment, IConfigurationRoot configuration)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.Debug()
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri("http://23.100.86.9:9200/"))
+            {
+                AutoRegisterTemplate = true,
+                AutoRegisterTemplateVersion = AutoRegisterTemplateVersion.ESv6,
+                IndexFormat = $"{Assembly.GetExecutingAssembly().GetName().Name!.ToLower().Replace(".", "-")}-{environment?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM-dd}"
+            })
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
     }
 
 
@@ -49,5 +55,5 @@ public class Program
             {
                 webBuilder.UseStartup<Startup>();
             })
-         .UseSerilog();
+        .UseSerilog();
 }
