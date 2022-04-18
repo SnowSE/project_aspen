6153c57ba8351f8a394ff2ee53e7fdb77e56d4e1
Esplintk <59877903+Esplintk@users.noreply.github.com>
2022-04-18 15:09:25 -0600

    Fixed Log Issue on AdminController

--- src/api/Controllers/AdminController.cs
+++ src/api/Controllers/AdminController.cs
@@ -12,11 +12,12 @@ public class AdminController : ControllerBase
     private readonly ILogger<AdminController> log;
 
 
-    public AdminController(IDonationRepository donationRepository, IEventRepository eventRepository, IMapper mapper)
+    public AdminController(IDonationRepository donationRepository, IEventRepository eventRepository, IMapper mapper, ILogger<AdminController> log)
     {
         this.donationRepository = donationRepository;
         this.eventRepository = eventRepository;
         this.mapper = mapper;
+        this.log = log;
     }
 
     [HttpGet, Authorize(Roles = AspenAdminRole)]
