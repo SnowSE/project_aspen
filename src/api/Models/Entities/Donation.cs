6531385a02aa0b722cce370c1470657ccd122e19
Jonathan Allen <jallen@snow.edu>
2022-04-18 16:03:08 -0600

    DtoDonation mapping to include person.name and team.name

--- src/api/Models/Entities/Donation.cs
+++ src/api/Models/Entities/Donation.cs
@@ -4,9 +4,12 @@ public record Donation
 {
     public long ID { get; init; }
     public long EventID { get; init; }
+    public Event Event { get; init; }
     public long? TeamID { get; init; }
+    public Team Team { get; init; }
     public long? PersonID { get; init; }
+    public Person Person { get; init; }
     public DateTime Date { get; init; }
     public decimal Amount { get; init; }
-    public bool IsPending { get; init; } = false;
+    public bool IsPending { get; init; } = true;
 }
