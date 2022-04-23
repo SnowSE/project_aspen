016a6487c8283bc3d85b8257465203f7b007e24f
Durli Hojayeva <Durli.Hojayeva@snow.edu>
2022-04-22 19:32:41 -0600

    showing donated current amount in team detail Page

--- src/mobile/AspenMobile/AspenMobile/ViewModels/TeamDetailViewModel.cs
+++ src/mobile/AspenMobile/AspenMobile/ViewModels/TeamDetailViewModel.cs
@@ -17,12 +17,30 @@ namespace AspenMobile.ViewModels
     {
         [ObservableProperty]
         public DtoTeam team;
+        [ObservableProperty]
+        public decimal currentDonatedAmount;
 
         public TeamDetailViewModel()
         {
+            GetTeamInfoAsync(36,5);
         }
 
         private int teamId;//needs to be set by naviagation parameter
+        private int eventId;
+
+        public int EventId
+        {
+            get
+            {
+                return eventId;
+            }
+            set
+            {
+                eventId = value;
+                
+
+            }
+        }
         public int TeamId
         {
             get
@@ -32,7 +50,7 @@ namespace AspenMobile.ViewModels
             set
             {
                 teamId = value;
-                GetTeamInfoAsync(value);
+                
 
             }
         }
@@ -42,7 +60,7 @@ namespace AspenMobile.ViewModels
         private string errorMessage;
 
 
-        public async Task GetTeamInfoAsync(int teamId)
+        public async Task GetTeamInfoAsync(int teamId, int eventId)
         {
             var httpClient = new HttpClient();
             try
@@ -50,8 +68,13 @@ namespace AspenMobile.ViewModels
                 var server = Preferences.Get(Constants.CurrentServer, null) ?? throw new Exception("No server address set");
                 var uri = new Uri($"{server}/api/teams/{teamId}");
                 var team = await httpClient.GetFromJsonAsync<DtoTeam>(uri);
+                var currentDonationUri = new Uri($"{server}/api/donations/{eventId}/{teamId}");
 
                 Team = team;
+                var dtoDonation = await httpClient.GetFromJsonAsync<decimal>(currentDonationUri);
+
+                
+                CurrentDonatedAmount = dtoDonation;
             }
             catch (Exception ex)
             {
