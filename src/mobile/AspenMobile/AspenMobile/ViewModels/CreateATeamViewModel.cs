99cb4b3020bd2619fc4494b8dd2471046f78c3e3
Esplintk <59877903+Esplintk@users.noreply.github.com>
2022-04-25 15:13:04 -0600

    FIxed code location

--- src/mobile/AspenMobile/AspenMobile/ViewModels/CreateATeamViewModel.cs
+++ src/mobile/AspenMobile/AspenMobile/ViewModels/CreateATeamViewModel.cs
@@ -13,30 +13,6 @@ namespace AspenMobile.ViewModels
 {
     internal partial class CreateATeamViewModel
     {
-        public int id { get; set; }
-        public string date { get; set; }
-        public string title { get; set; }
-        public string location { get; set; }
-        public string description { get; set; }
-        public string primaryImageUrl { get; set; }
-        public double donationTarget { get; set; }
-        private readonly HttpClient httpClient = new();
-        private string current;
-        private Lazy<HttpClient> apiClient = new Lazy<HttpClient>(() => new HttpClient());
-        public CreateATeamViewModel()
-        {
-            current = Preferences.Get(Constants.CurrentServer, null);
-            if (current == null)
-            {
-                Shell.Current.GoToAsync($"{nameof(SettingsPage)}");
-            }
-            apiClient.Value.BaseAddress = new Uri($"{current}/api/");
-        }
-
-        [ICommand]
-        public async Task SubmitNewEventAsync()
-        {
-           // await httpClient.PostAsync($"{current}/api/events");
-        }
+        
     }
 }
