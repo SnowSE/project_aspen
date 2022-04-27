3e82812a4ca270e5bb3884962dcfa4d4f511b477
Jonathan Allen <jallen@snow.edu>
2022-04-27 14:43:24 -0600

    Finalized the create new event page

--- src/mobile/AspenMobile/AspenMobile/Views/CreateNewEventPage.xaml.cs
+++ src/mobile/AspenMobile/AspenMobile/Views/CreateNewEventPage.xaml.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using AspenMobile.ViewModels;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +11,7 @@ namespace AspenMobile.Views
         public CreateNewEventPage()
         {
             InitializeComponent();
+            BindingContext = new CreateNewEventViewModel();
         }
     }
 }
