bd324d2421d0f6f2957b02df8dc2930762657890
Esplintk <59877903+Esplintk@users.noreply.github.com>
2022-11-04 16:03:37 -0600

    Created my branch and created pages folder and moved homepage to the folder

---
+++ src/combined/ClientApp/src/Pages/Home.tsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import {Box, Button, Typography} from '@mui/material';
+
+export function Home() {
+    return (
+        <Box>
+            <Typography>This is the Home Page</Typography>
+        </Box>
+    );
+}
