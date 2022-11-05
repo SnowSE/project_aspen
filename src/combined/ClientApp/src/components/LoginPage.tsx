676ccfd78e1b48b0afeaba1b447e1c2b5e06c014
Durli Hojayeva <durli.hojayeva@students.snow.edu>
2022-11-05 13:03:01 -0600

    Added backward navigation button

--- src/combined/ClientApp/src/components/LoginPage.tsx
+++ src/combined/ClientApp/src/components/LoginPage.tsx
@@ -1,12 +1,30 @@
 ﻿import React, { useState } from 'react';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
 
 export function LoginPage() {
+    const navigate = useNavigate();
+
     return (
         <div>
-            <p>I am Login Page</p>
+            <button onClick={() => navigate(-1)}>Go back 1 Page</button>
+            <div>
+                <nav>                   
+                       <Link to="/"></Link>                       
+                </nav>
+                <Routes>
+                    <Route path="/" element={<App />} />
+                </Routes>
+            </div>
+            
         </div >);
 
 
 
     
+}
+
+
+function App() {
+    return (
+        <div></div>);
 }
