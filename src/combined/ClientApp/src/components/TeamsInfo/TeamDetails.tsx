5874389af912769683ea92223da11ddae721521d
durli.hojayeva <hojayevadurli@gmail.com>
2022-11-14 20:40:57 -0700

    created team Details page and added routing to it

---
+++ src/combined/ClientApp/src/components/TeamsInfo/TeamDetails.tsx
@@ -0,0 +1,43 @@
+﻿import { TeamCard } from "./Interfaces";
+
+
+//type TeamCardProps = {
+//    project: TeamCard
+//}
+
+
+export function TeamDetails() {
+    return (
+        <div>
+            <h1>I am in Team details page</h1>
+            <h1>I am in Team details page</h1>
+            <h1>I am in Team details page</h1>
+            <h1>I am in Team details page</h1>
+            <h1>I am in Team details page</h1>
+            <h1>I am in Team details page</h1>
+            <h1>I am in Team details page</h1>
+            <h1>I am in Team details page</h1>
+            <h1>I am in Team details page</h1>
+            <h1>I am in Team details page</h1>
+
+            {/*{teamsList.map((t: any) => {*/}
+            {/*    return (*/}
+            {/*        <TeamCard*/}
+            {/*            name={t.name}*/}
+            {/*            id={t.id}*/}
+            {/*            description={t.description}*/}
+            {/*            mainImage={t.mainImage}*/}
+            {/*            ownerID={t.ownerID}*/}
+            {/*            owner={t.owner}*/}
+            {/*            eventID={t.eventID}*/}
+            {/*            donationTarget={t.donationTarget}*/}
+            {/*            key={t.id}*/}
+            {/*        />*/}
+
+            {/*    )*/}
+            {/*})}        */}
+
+        </div>
+    );
+}
+    
