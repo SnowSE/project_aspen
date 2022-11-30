59368ba58d9c05d6d40c0291a0274401a73bdc47
Jonathan Allen <jallen@snow.edu>
2022-11-30 14:37:46 -0700

    get POST request working to upload image to asset controller

--- src/combined/ClientApp/src/components/Team/CreateTeamForm.tsx
+++ src/combined/ClientApp/src/components/Team/CreateTeamForm.tsx
@@ -1,31 +1,18 @@
-import Button from "@mui/material/Button";
-import React, { useEffect, useState } from "react";
+﻿import Button from "@mui/material/Button";
+import React, { useState } from "react";
 import axios from 'axios'
 import { Col, Form, FormGroup, FormText, Input, Label, Row } from "reactstrap";
-import { EventsService } from "../../services/Events/EventsService";
-import { authService } from "../../services/authService";
-import { TextField } from "@mui/material";
-import { useNavigate } from "react-router-dom";
-
-
 
 const CreateTeamForm = () => {
-    // console.log('REACT_APP_BASE_URL', process.env.REACT_APP_BASE_URL)
-    // console.log('BASE_URL', process.env.BASE_URL)
-    // console.log('PUBLIC_URL', process.env.PUBLIC_URL)
-    // console.log('everything', process.env)
-
-
-    const navigate = useNavigate()
+    console.log('REACT_APP_BASE_URL', process.env.REACT_APP_BASE_URL)
+    console.log('BASE_URL', process.env.BASE_URL)
+    console.log('PUBLIC_URL', process.env.PUBLIC_URL)
+    console.log('everything', process.env)
 
     const [teamName, setTeamName] = useState<string>('')
     const [teamDescription, setTeamDescription] = useState<string>('');
     const [donationGoal, setDonationGoal] = useState<number>(0);
-    const [image, setImage] = useState<string>('')
-    const [disableSubmit, setDisableSubmit] = useState<boolean>(true)
-
-
-    const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false)
+    const [image, setImage] = useState<File>()
 
     const config = {
         headers: { Authorization: `Bearer ${localStorage.getItem("access_token")}` }
@@ -40,68 +27,83 @@ const CreateTeamForm = () => {
         donationTarget: number
     }
 
-
     const createTeamHandler = async (event: React.FormEvent) => {
         event.preventDefault()
+        console.log(process.env.REACT_APP_BASE_URL)
         var currentUserUrl = process.env.REACT_APP_BASE_URL + "/api/User"
         var eventsUrl = process.env.REACT_APP_BASE_URL + "/api/events"
-        const currentUser = await axios.get(currentUserUrl, config)
-        const keycloakUser = await authService.getUser();
-        const events = await EventsService.GetEventsViaAxios()
+        var assetsUrl = process.env.REACT_APP_BASE_URL + "/api/asset"
 
+        if (!image) {
+            return
+        }
+
+        const data = new FormData();
+        data.append('asset', image, image.name);
+        const imageResponse = await fetch(assetsUrl, {
+            method: 'POST',
+            body: data,
+            headers: {
+                Authorization: `Bearer ${localStorage.getItem("access_token")}`                
+            }
+        })
+        const result = await imageResponse.json()
+        console.log('upload result:', result)
         
-
+        const currentUser = await axios.get(currentUserUrl, config)
+        const events = await axios.get(eventsUrl)
 
         let newTeam: team = {
             name: teamName,
             description: teamDescription,
-            mainImage: image,
+            mainImage: result.data,
             ownerID: Number(currentUser.data.id),
-            eventID: events[0].id!,
-            donationTarget: donationGoal!
+            eventID: events.data[0].id,
+            donationTarget: donationGoal
         }
 
-
-        
         var teamUrl = process.env.REACT_APP_BASE_URL + "/api/teams"
-
         const res = await axios.post(teamUrl, newTeam, config)
             .then((response) => { })
             .catch((error) => { console.log(error.response.data) })
 
         setTeamDescription('')
         setTeamName('')
-
-        navigate("/TeamsListPage")
+        setDonationGoal(0)
     }
 
-    useEffect(()=> {
-        if (teamName.trim().length != 0 && teamDescription.trim().length != 0 && donationGoal! > 0) {
-            setDisableSubmit(false)
+    const imageOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files) {
+            setImage(e.target.files[0]);
         }
-        else {
-            setDisableSubmit(true)
-        }
-async function currentUser() {
-            var user = await authService.getUser()
-            if (user == null){
-                setIsLoggedIn(false)
-            }
-            else{
-                setIsLoggedIn(true)
-            }
-        }
-        currentUser()
-    }, [teamName, teamDescription, donationGoal])
-
-
-
-
+    }
 
     return (
-        <div style={{display:'flex', justifyContent:'center'}}>
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
 
-        {isLoggedIn ? <Form onSubmit = {createTeamHandler} style={{width:'90vw', border:'solid #673ab7', borderRadius:'30px'}}>
+            <Form onSubmit={createTeamHandler} style={{ width: '90vw', border: 'solid #673ab7', borderRadius: '30px' }}>
+                <FormGroup>
+                    <Row style={{ display: 'flex', justifyContent: 'center' }}>
+                        <Col md={6} xs={8}>
+
+                            <Label
+                                for="exampleFile"
+                                sm={2}
+                            >
+                            </Label>
+                            <Input
+                                id="exampleFile"
+                                name="file"
+                                type="file"
+                                placeholder="Team Logo"
+                                onChange={imageOnChange}
+                            />
+                            <FormText>
+                                Select an image that will be displayed as your team's logo
+                            </FormText>
+                        </Col>
+                    </Row>
+                </FormGroup>
                 <Row style={{ display: 'flex', justifyContent: 'center' }}>
                     <Col md={6} xs={8}>
                         <FormGroup>
@@ -133,9 +135,8 @@ async function currentUser() {
                             </Label>
                             <Input
                                 id="exampleText"
-                                name="teamDescription"
+                                name="text"
                                 type="textarea"
-                                placeholder="Team Description"
                                 value={teamDescription}
                                 onChange={event => setTeamDescription(event.target.value)}
                             />
@@ -158,18 +159,14 @@ async function currentUser() {
                                 value={donationGoal}
                                 onChange={event => setDonationGoal(Number(event.target.value))}
                             />
-
                         </Col>
                     </Row>
                 </FormGroup>
                 <Col md={12} xs={8} style={{ display: 'flex', justifyContent: 'center' }}>
 
-                    <Button variant='contained' disabled = {disableSubmit} sx={{ backgroundColor: 'orange' }} type="submit" >Submit</Button> 
-
+                    <Button variant='contained' sx={{ backgroundColor: 'orange' }} type="submit" >Submit</Button>
                 </Col>
             </Form>
-                        
-        : <h1>Not logged in</h1> }
         </div>
 
     );
