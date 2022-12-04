d93eb5189864d1cfc1844cefd295721880bd829c
sergio-ayala123 <venturian6@gmail.com>
2022-12-03 20:20:20 -0700

    lint fix

--- src/combined/ClientApp/src/Pages/Event/CreateEventPage.spec.tsx
+++ src/combined/ClientApp/src/Pages/Event/CreateEventPage.spec.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
+import { render, screen} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect'
 import CreateEventPage from './CreateEventPage';
 import CreateEventForm from '../../components/Event/CreateEventForm';
