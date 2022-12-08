9e037532bab57e40394cce8257638c797d25ed5d
durli.hojayeva <hojayevadurli@gmail.com>
2022-12-07 18:52:35 -0700

    some merge conflicts

--- src/combined/ClientApp/src/components/TeamsInfo/LoggedInUser.tsx
+++ src/combined/ClientApp/src/components/TeamsInfo/LoggedInUser.tsx
@@ -3,6 +3,7 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { Col, Form, FormGroup, FormText, Input, Label, Row } from "reactstrap";
+import registration from "../../JsModels/registration";
 //import PersonRegistrations from "../../JsModels/personRegistrations";
 //import registration from "../../JsModels/registration";
 
