7897bb2bc2f323dccc3c52737061169fdad3ad6c
Jonathan Allen <jallen@snow.edu>
2022-12-09 14:59:24 -0700

    improve process of working with database snapshots (ticket #343)

--- dev-resources/api/dump_db.sh
+++ dev-resources/api/dump_db.sh
@@ -1,1 +1,1 @@
-docker exec -t api_db pg_dump -c -U aspen aspen > backup.sql
+docker exec -t localdev_api_db pg_dump -c --if-exists -U aspen aspen > backup.sql
--- src/combined/MigrationApplier.cs
+++ src/combined/MigrationApplier.cs
@@ -27,10 +27,9 @@
                 if (System.Diagnostics.Debugger.IsAttached)
                 {
                     logger.LogWarning("Maybe it's a connection string issue, or the database is not up?\n");
-                    logger.LogWarning(@"If so, try these commands FROM THE src/combined DIRECTORY:
+                    logger.LogWarning(@"If so, try this command FROM THE dev-resources/api DIRECTORY:
 
-dotnet user-secrets set ""ASPEN_CONNECTION_STRING"" ""server=localhost; port=5434; database=postgres; user id=postgres; password=P@assword1""
-docker run -d --name pg -e POSTGRES_USER=postgres -e POSTGRES_PASSWORD=P@assword1 -p 5434:5432 postgres
+docker compose up 
 
 ");
                 }
