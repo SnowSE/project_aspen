392b1deb11b4008ee44581d5004b6b239ac4a44c
jayse19 <jayser19@hotmail.com>
2022-12-09 16:28:54 -0700

    Dynamic lists for teams encompassed by a team accordion

--- src/combined/ClientApp/src/Pages/SiteAdmin/SiteAdmin.tsx
+++ src/combined/ClientApp/src/Pages/SiteAdmin/SiteAdmin.tsx
@@ -4,53 +4,65 @@ import { EventContext } from "../../App";
 import EventEditDeleteForm from "../../components/AdminComponents/EventEditDeleteForm";
 import TeamMembersListAccordian from "../../components/AdminComponents/TeamMembersListAccordian";
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import { getTeamsList } from "../../components/TeamsInfo/TeamServices";
+import Team from "../../JsModels/team";
 
 
 
 const SiteAdmin = () => {
     const { currentEvent } = useContext(EventContext);
-    
+    const [teamsList, setTeams] = useState<Team[]>();
     const [showEditEvent, setShowEditEvent] = useState(true);
+
     useEffect(() => {
-    }, [currentEvent]);
+        const fetchData = async () => {
+            if (!currentEvent.id) {
+                console.log("No current event found!")
+                return;
+            }
+            var teamsList = await getTeamsList(currentEvent.id)
+            var jsonTeams: Team[] = JSON.parse(JSON.stringify(teamsList));
+            setTeams(jsonTeams)
+        }
+        fetchData()
+    }, [currentEvent])
 
     return (
 
         <Box>
             <Box>
-            {
-                showEditEvent === true ? <Typography>{currentEvent?.title}</Typography> : <EventEditDeleteForm />
-            }
+                {
+                    showEditEvent === true ? <Typography>{currentEvent?.title}</Typography> : <EventEditDeleteForm />
+                }
                 <Button type='button' variant='contained' onClick={() => setShowEditEvent((prevState) => !prevState)}>
                     {
                         showEditEvent === true ? "Edit" : "Close"
                     }
                 </Button>
             </Box>
-            <Box sx={{ pt: '4rem' }} >
-                <Accordion>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon />}
-                    >
-                        <Typography>
-                            Example Non-dynamic Team name
-                        </Typography>
-                    </AccordionSummary>
-                    <TeamMembersListAccordian />
-                </Accordion>
-                <Accordion>
-                    <AccordionSummary
-                        expandIcon={<ExpandMoreIcon />}
-                    >
-                        <Typography>
-                            Example Non-dynamic Team name2
-                        </Typography>
-                    </AccordionSummary>
-                    <TeamMembersListAccordian />
-                </Accordion>
-            </Box>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon/> }
+                >
+                    <Typography> Teams </Typography>
+                </AccordionSummary>
+            {teamsList?.map((t: any, id) => {
+                return (
+                        <Accordion>
+                            <AccordionSummary
+                                expandIcon={<ExpandMoreIcon />}
+                            >
+                                <Typography>
+                                    {t.name}
+                                </Typography>
+                            </AccordionSummary>
+                            <TeamMembersListAccordian />
+                        </Accordion>
+               )})}
+            </Accordion>
             
         </Box>
+        
     );
 };
 
