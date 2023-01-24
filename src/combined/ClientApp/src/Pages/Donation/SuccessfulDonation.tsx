ef61f05b50b7c919a4d272339dec4b9b9cd3f9f0
Jayse Baruch <jayse19@hotmail.com>
2023-01-23 22:32:00 -0700

    most of the recipt page is done

--- src/combined/ClientApp/src/Pages/Donation/SuccessfulDonation.tsx
+++ src/combined/ClientApp/src/Pages/Donation/SuccessfulDonation.tsx
@@ -1,39 +1,85 @@
-import { useEffect } from "react";
-import { useNavigate, useParams } from "react-router-dom";
+import { useContext } from "react";
+import {useNavigate, useParams } from "react-router-dom";
 import Confetti from 'react-confetti'
-
+import { Box, Button, Divider, Typography } from "@mui/material";
+import { EventContext } from "../../App";
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import SharingButton from "../../components/Share/SharingButton";
 
 
 const SuccessfulDonation = () => {
-    const navigate = useNavigate()
-
     const { personName, teamName } = useParams()
-
-    useEffect(() => {
-        setTimeout(() => {
-            navigate("/")
-        }, 4000);
-    })
-
+    const { currentEvent } = useContext(EventContext);
+    const navigate = useNavigate();
 
     return (
-        <>
+        <Box>
+            <Box>
+                <CheckCircleOutlineIcon />
+            </Box>
             {personName === "Anonymous" ?
-                <div>
-                    <Confetti />
+                <Box>
+                    <Confetti  />
                     <h1 style={{ textAlign: 'center' }}>Thank you stranger for your donation</h1>
-                </div>
+                </Box>
                 :
-                <div>
+                <Box>
                     <Confetti />
 
                     <h1 style={{ textAlign: 'center' }}>
-                        Thank you {personName} For your donation to the team: {teamName}
+                        Thank you {personName} for your donation to the {teamName} team during {currentEvent?.title} event!
                     </h1>
-                </div>
+                </Box>
             }
+            <Divider />
+            <Box>
+                <Typography>
+                    Summary:
+                </Typography>
+            </Box>
+            <Box>
+                <Typography>
+                    Contribution to {currentEvent?.Title }: (Donation Value to be added)
+                </Typography>
+            </Box>
+            <Divider />
+            <Box>
+                <Typography>
+                    Date: get current date
+                </Typography>
+            </Box>
+            <Box>
+                <Typography>
+                    Transaction ID: 
+                </Typography>
+            </Box>
+            <Divider />
+            <Box>
+                <Typography>
+                    name
+                </Typography>
+            </Box>
+            <Box>
+                <Typography>
+                    email
+                </Typography>
+            </Box>
+            <Box>
+                <Typography>
+                    phone number
+                </Typography>
+            </Box>
 
-        </>
+            <Box className="DonateButtonPosition">
+                <Button onClick={() => navigate("/")} variant='contained'
+                    data-testid={'homePageBtn'}
+                    id={"homePageBtn"}>
+                    Return to home page
+                </Button>
+                <SharingButton />
+            </Box>
+
+        </Box>
     );
 }
 
