0b49c9f3c187692a7a0b4bb451ed643ed7b498ce
Jesse Madsen <madsencad@gmail.com>
2023-01-29 08:55:30 -0700

    guid started on ID

--- src/combined/ClientApp/src/components/Share/SharingButtonCustomLink.tsx
+++ src/combined/ClientApp/src/components/Share/SharingButtonCustomLink.tsx
@@ -1,60 +1,68 @@
 import { RWebShare } from "react-web-share";
 import { Button } from '@mui/material';
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { EventContext } from '../../App';
 import axios from "axios";
-import Link from "../../JsModels/link";
 
-const SharingButtonCustomLink = () => {
+
+const SharingButtonCustomLink: React.FC = () => {
     const BaseUrl = process.env.PUBLIC_URL
     const shareUrl = window.location.href;
+    const [urlValid, setUrlValid] = useState<boolean>(false);
     const { currentEvent } = useContext(EventContext);
     const [link, setLink] = useState<string>(shareUrl);
     const config = {
         headers: { Authorization: `Bearer ${localStorage.getItem("access_token")}` }
     };
 
-
-    const buildLink = async () => {
+    async function buildLink() {
         try {
             const currentUser = await axios.get(process.env.PUBLIC_URL + "/api/User", config);
             if (currentUser.data.id !== null) {
+                let responseID = -1;
                 await axios.post(`${process.env.PUBLIC_URL}/api/links`,
                     {
+                        Id :new Guid(),
                         EventId: currentEvent.id,
                         PersonID: currentUser.data.id,
                         Date: new Date(),
                         LinkURL: shareUrl
                     }).then((response) => {
-
-                        setLink(shareUrl + "/links/" + response.data.id);
+                        responseID = response.data.id
                     })
-                    .catch((error) => { console.log("There was an error", error.response.data) })
+                    .catch((error) => { console.log("There was an error", error.response.data); });
+                setLink(`${shareUrl} /links/ ${responseID}`);
             }
         } catch (error) {
             console.log(`$Error on building link, error:${error}`);
         }
-    };
-    const getLink = async () => {
-        await buildLink()
-        
     }
+
+    const handleClick = async () => {
+        await buildLink();
+        setUrlValid(true);
+    }
+
+    useEffect(() => {
+        buildLink();
+    }, [urlValid]);
 
     return (
         <div>
             <RWebShare
-                data-testid={"shareModal"}
+                data-testid="shareModal"
                 data={{
                     text: "",
                     url: link,
                     title: "Name of Event Here"
                 }}
-                onClick={() => getLink()}
+                onClick={handleClick}
             >
                 <Button variant='contained' className="ShareButton">SHARE NOW</Button>
             </RWebShare>
+            
         </div>
     );
-};
+}
 
 export default SharingButtonCustomLink;
