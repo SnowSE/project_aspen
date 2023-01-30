4c0716643964edcc593ee4467c548766455eca5e
Jesse Madsen <madsencad@gmail.com>
2023-01-30 13:31:22 -0700

    app test added back

--- src/combined/ClientApp/src/App.spec.tsx
+++ src/combined/ClientApp/src/App.spec.tsx
@@ -1,23 +1,23 @@
-//import {render} from '@testing-library/react';
-//import '@testing-library/jest-dom/extend-expect'
-//import App, { EventContext } from './App'
+import {render} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
+import App, { EventContext } from './App'
 
-//const mockedUsedNavigate = jest.fn();
+const mockedUsedNavigate = jest.fn();
 
-//jest.mock('react-router-dom', () => ({
-//    ...jest.requireActual('react-router-dom') as any,
-//    useNavigate: () => mockedUsedNavigate,
-//}));
+jest.mock('react-router-dom', () => ({
+    ...jest.requireActual('react-router-dom') as any,
+    useNavigate: () => mockedUsedNavigate,
+}));
 
-//describe('App.tsx tests', () => {
-//    beforeEach(() => {
-//        render(<App />);
-//    })
+describe('App.tsx tests', () => {
+    beforeEach(() => {
+        render(<App />);
+    })
 
-//    test('App.tsx renders', () => {
-//        var temp = expect(EventContext.Provider).toBeInTheDocument
-//        console.log("App.tsx:" + temp)
-//    });
+    test('App.tsx renders', () => {
+        var temp = expect(EventContext.Provider).toBeInTheDocument
+        console.log("App.tsx:" + temp)
+    });
     
 
-//});
+});
