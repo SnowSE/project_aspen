e7bf21fcb159a5909219c3392888132aa635ec09
jm <madsencad@gmail.com>
2023-01-31 09:20:17 -0700

    Link Contorller Test working

--- src/api-tests/ControllerTests/LinkContorllerTests.cs
+++ src/api-tests/ControllerTests/LinkContorllerTests.cs
@@ -1,6 +1,9 @@
-﻿using Api.Extensions;
+﻿using Api.Controllers;
+using Api.Extensions;
 using Microsoft.Extensions.Logging;
+using shared.DtoModels;
 using Tests;
+using Tests.ControllerTests;
 using Tests.Steps;
 
 namespace Api.Tests.Controllers
@@ -8,8 +11,12 @@ namespace Api.Tests.Controllers
     [TestFixture]
     public class LinkControllerTests
     {
+        private EventController eventController;
+        private DtoEvent testEvent;
         private LinkController linkController;
-        private DtoLink testLink;
+        private DtoLink testLinkLoggedInUser;
+        private DtoLink testGetLink;
+        private DtoPerson personOne;
 
         public static LinkController GetLinkController()
         {
@@ -20,37 +27,55 @@ namespace Api.Tests.Controllers
         }
         
         [SetUp]
-        public void SetUp()
+        public async Task SetUp()
         {
-            linkController = GetLinkController();
-            testLink = (await linkController.Add(new DtoLink
+            ///Setup Event
+            eventController = EventControllerTest.GetEventController();
+            testEvent = (await eventController.Add(new DtoEvent
             {
-                Identifier = "link1",
-                Url = "https://www.google.com",
-                Description = "Google",
-                ImageUrl = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png"
+                Date = new DateTime(1775, 7, 2).SetKindUtc(),
+                Description = "Independence",
+                Location = "Philly",
+                Title = "Sign Here",
+                MainImage = "july4.jpg"
             })).Value;
-        })).Value;
+            
+            //Setup Person
+            var personController = PersonControllerTest.GetPersonController();
+            personOne = (await personController.Add(new DtoPerson { Name = "Team 1 Owner" })).Value;
+            
+            linkController = GetLinkController();
+            testLinkLoggedInUser = (await linkController.Add(new DtoLink
+            {
+                EventID = testEvent.ID,
+                PersonID = personOne.ID,
+                Date = new DateTime(1775, 7, 2).SetKindUtc(),
+                LinkURL = "http://google.com",
+                LinkIdentifer = "77d475fd-7107-4e1a-9219-bde7e0e5d007"
+            })).Value;
+
+
+
         }
 
         [Test]
-        public async Task Get_ReturnsLink_WhenLinkIdentifierExists()
+        public async Task CanCreateLinkLoggedInUser()
         {
-            // Arrange
-            var linkIdentifier = "link1";
-            var link = new Link { Identifier = linkIdentifier };
-            var dtoLink = new DtoLink { Identifier = linkIdentifier };
-            _linkRepositoryMock.Setup(x => x.GetLinkByIdentiferAsync(linkIdentifier)).ReturnsAsync(link);
-            _mapperMock.Setup(x => x.Map<DtoLink>(link)).Returns(dtoLink);
+            testLinkLoggedInUser.ID.Should().NotBe(0);
+            testLinkLoggedInUser.EventID.Should().Be(1);
+            testLinkLoggedInUser.LinkURL.Should().Be("http://google.com");
+            testLinkLoggedInUser.LinkIdentifer.Should().Be("77d475fd-7107-4e1a-9219-bde7e0e5d007");
+        }
 
-            // Act
-            var result = await _controller.Get(linkIdentifier);
+        [Test]
+        public async Task CanGetLinkFromIdentifier()
+        {
 
-            // Assert
-            result.Should().BeOfType<DtoLink>();
-            result.Identifier.Should().Be(linkIdentifier);
-            _linkRepositoryMock.Verify(x => x.GetLinkByIdentiferAsync(linkIdentifier), Times.Once);
-            _mapperMock.Verify(x => x.Map<DtoLink>(link), Times.Once);
+            testGetLink = (await linkController.Get("77d475fd-7107-4e1a-9219-bde7e0e5d007"));
+            testGetLink.ID.Should().Be(1);
+            testGetLink.EventID.Should().Be(1);
+            testGetLink.LinkURL.Should().Be("http://google.com");
+            testGetLink.LinkIdentifer.Should().Be("77d475fd-7107-4e1a-9219-bde7e0e5d007");
         }
 
     }
