d8f0581ae5f235576cc58346a0fc9119d33eade8
Durli <hojayevadurli@gmail.com>
2023-02-08 22:01:55 -0700

    Some

--- src/combined/ClientApp/src/components/TeamsInfo/TeamDetails.tsx
+++ src/combined/ClientApp/src/components/TeamsInfo/TeamDetails.tsx
@@ -150,6 +150,35 @@ export function TeamDetails() {
                   }
               })()
           }  
+          {/*{(() => {*/}
+          {/*    if (currentTeam?.isPublic === true) {*/}
+          {/*        return (*/}
+          {/*            <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'flex-end', float: "right" }}>*/}
+          {/*                <Button*/}
+          {/*                    onClick={() =>*/}
+          {/*                        loggedInUSer*/}
+          {/*                            ? navigate({*/}
+          {/*                                pathname: "/LoggedInUser",*/}
+          {/*                                search: `?${createSearchParams({*/}
+          {/*                                    teamId: `${tId}`,*/}
+          {/*                                    userId: `${loggedInUserId}`,*/}
+          {/*                                })}`,*/}
+          {/*                            })*/}
+          {/*                            : authService.signinRedirect()*/}
+          {/*                    }*/}
+          {/*                    sx={{ backgroundColor: "orange", m: 2, fontSize: "10px" }}*/}
+          {/*                >*/}
+          {/*                    Join Our Team*/}
+          {/*                </Button>*/}
+          {/*            </Grid>*/}
+          {/*        )*/}
+          {/*    } else {*/}
+          {/*        return (*/}
+          {/*            <p>This is Private Team</p>*/}
+          {/*        )*/}
+          {/*    }*/}
+          {/*})()*/}
+          {/*}  */}
       
       {currentTeam?.id}
       {currentTeam?.ownerID}
