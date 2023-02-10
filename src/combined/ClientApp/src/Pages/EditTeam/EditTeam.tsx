71e8fa72a758bf36b27be4916437bfc0bd906ffc
durli.hojayeva <hojayevadurli@gmail.com>
2023-02-10 00:47:32 -0700

    included just team name to edit it

--- src/combined/ClientApp/src/Pages/EditTeam/EditTeam.tsx
+++ src/combined/ClientApp/src/Pages/EditTeam/EditTeam.tsx
@@ -1,4 +1,5 @@
-﻿import React, { useEffect, useState, useContext } from "react";
+﻿import axios from "axios";
+import React, { useEffect, useState, useContext } from "react";
 import { useSearchParams } from "react-router-dom";
 
 const EditTeam = () => {
@@ -20,11 +21,59 @@ const EditTeam = () => {
 
     const api = process.env.PUBLIC_URL + `/api/teams/${tId}`;
     const [currentTeam, setCurrentTeam] = useState<any>();
+    useEffect(() => {
+        const fetchTeam = async () => {
+            const response = await fetch(api);
+            const data = await response.json();
+            setCurrentTeam(data);
+        };
+
+        const callServise = async () => {
+            await fetchTeam();
+        };
+        callServise();
+
+    }, [api]);
+
    
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();        
+            try {
+                const response = await axios.put(api, currentTeam);
+                console.log(response);
+                alert("Team information updated successfully!");
+            } catch (error) {
+                console.error(error);
+                alert("An error occurred while updating the team information.");
+            }      
+
+    };
+
+
+
     return (
         <div>
-            "I am in Edit team page"
+            {currentTeam ? (
+                <form onSubmit={handleSubmit}>
+                    <label>
+                        Team Name:
+                        <input
+                            type="text"
+                            value={currentTeam.name}
+                            onChange={(event) =>
+                                setCurrentTeam({ ...currentTeam, name: event.target.value })
+                            }
+                        />
+                    </label>
+                    {/* Add other fields to display and edit the team information */}
+                    <button type="submit">Save</button>
+                </form>
+            ) : (
+                "Loading..."
+            )}
         </div>
     );
+
 }
 export default EditTeam;
