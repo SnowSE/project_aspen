930158c41811d416d5af99ccf528ff191f1d1e9b
Esplintk <59877903+Esplintk@users.noreply.github.com>
2023-02-17 12:50:22 -0700

    took out buttons

--- src/combined/ClientApp/src/components/TeamsInfo/Interfaces.tsx
+++ src/combined/ClientApp/src/components/TeamsInfo/Interfaces.tsx
@@ -32,59 +32,19 @@ export const TeamCard = ({ id, name, description, mainImage, ownerID, eventID, d
             <div style={{ paddingTop: "1rem", justifyContent: "flex-start" }}>
                 <div className="d-flex justify-content-start" >
                     <div>
-                        <Card style={{ width: "30rem" }}
-                           
-                        >
-                            <div className="card text-start" style={{ backgroundColor: 'purple' }}>
-                                <div className="card-header" style={{ fontSize: '50px', color: 'white' }}>{name}
-
-                                <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'flex-end', float: "right" }}>
-                                        <Button onClick={() => {
+                        <Card style={{ width: "30rem" }}>
+                            <div className="card text-start" style={{ backgroundColor: '#673ab7' }}>
+                                <Button sx={{fontSize: '25px', color: 'white'}} onClick={() => {
                                             navigate({
                                                 pathname: '/TeamDetails',
                                                 search: `?${createSearchParams({
                                                     teamId: `${id}`,
                                                     ownerID: `${ownerID}`
-
                                                 })}`
                                             })
-                                        }}
-                                        sx={{ backgroundColor: 'orange', m: 2, fontSize: '10px' }}>Learn About Our Team</Button>
-                                </Grid>
-                                    {
-                                        (() => {
-                                            if ( isPublic===true) {
-                                                return (
-                                                    <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'flex-end', float: "right" }}>
-                                                        <Button onClick={() => loggedInUSer ?
-                                                            navigate({
-                                                                pathname: "/LoggedInUser",
-                                                                search: `?${createSearchParams({
-                                                                    teamId: `${id}`,
-                                                                    userId: `${loggedInUserId}`
-
-                                                                })}`
-                                                            })
-                                                            : authService.signinRedirect()}
-                                                            sx={{ backgroundColor: 'orange', m: 2, fontSize: '10px' }}  >Join Our Team</Button>
-                                                    </Grid>
-                                                )
-                                            }  else {
-                                                return (
-                                                    <p>This is Private Team</p>
-                                                )
-                                            }
-                                        })()
-                                    }  
-                                   
-
-                                  
-
-                                </div>
+                                        }}>{name}</Button>
                             </div>
-
                         </Card>
-                       
                     </div>
                 </div>
             </div>
