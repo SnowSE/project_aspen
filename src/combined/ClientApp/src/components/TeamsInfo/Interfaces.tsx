1dda42e94d5244111e5674e9379cbe7691f1515d
Esplintk <59877903+Esplintk@users.noreply.github.com>
2023-02-27 18:54:40 -0700

    Finally Fixed the teams lists, no linting errors or tests failing either. So all good

--- src/combined/ClientApp/src/components/TeamsInfo/Interfaces.tsx
+++ src/combined/ClientApp/src/components/TeamsInfo/Interfaces.tsx
@@ -1,89 +1,46 @@
-import { Button, Card, Grid } from '@mui/material';
-import  React, {useState, useEffect} from 'react';
-import { createSearchParams, useNavigate } from 'react-router-dom';
-import Team from '../../JsModels/team';
-import { authService } from '../../services/authService';
-import axios from 'axios'
+import { Button, Card } from "@mui/material";
+import { createSearchParams, useNavigate } from "react-router-dom";
+import Team from "../../JsModels/team";
 
-export const TeamCard = ({ id, name, description, mainImage, ownerID, eventID, donationTarget,persons }: Team) => {
-    const navigate = useNavigate();
-    //const user = authService.isLoggedIn();
-    //{ localStorage.getItem("LoggedInUser") == "" ?}
+export const TeamCard = ({
+  id,
+  name,
+  description,
+  mainImage,
+  ownerID,
+  eventID,
+  donationTarget,
+  persons,
+}: Team) => {
 
-    const BaseUrl = process.env.PUBLIC_URL
-    const [loggedInUserId, setLoggedInUserId] = useState(0)
-
-    useEffect(() => {
-        const config = {
-            headers: { Authorization: `Bearer ${localStorage.getItem("access_token")}` }
-        };
-        const getUser = async () => {
-            await axios.get(BaseUrl + '/api/user', config).then((response) => {
-                setLoggedInUserId(response?.data?.id)
-            }).catch((error)=> {
-            })
-        }
-
-         getUser()
-    })
-
-    const loggedInUSer = localStorage.getItem("LoggedInUser")
-        return (
-            <div style={{ paddingTop: "1rem", justifyContent: "flex-start" }}>
-                <div className="d-flex justify-content-start" >
-                    <div>
-                        <Card style={{ width: "30rem" }}
-                           
-                        >
-                            <div className="card text-start" style={{ backgroundColor: 'purple' }}>
-                                <div className="card-header" style={{ fontSize: '50px', color: 'white' }}>{name}
-
-                                <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'flex-end', float: "right" }}>
-                                        <Button onClick={() => {
-                                            navigate({
-                                                pathname: '/TeamDetails',
-                                                search: `?${createSearchParams({
-                                                    teamId: `${id}`,
-                                                    ownerID: `${ownerID}`
-
-                                                })}`
-                                            })
-                                        }}
-                                        sx={{ backgroundColor: 'orange', m: 2, fontSize: '10px' }}>Learn About Our Team</Button>
-                                </Grid>
-                                    {
-                                        (() => {
-                                                return (
-                                                    <Grid item xs={4} sx={{ display: 'flex', justifyContent: 'flex-end', float: "right" }}>
-                                                        <Button onClick={() => loggedInUSer ?
-                                                            navigate({
-                                                                pathname: "/LoggedInUser",
-                                                                search: `?${createSearchParams({
-                                                                    teamId: `${id}`,
-                                                                    userId: `${loggedInUserId}`
-
-                                                                })}`
-                                                            })
-                                                            : authService.signinRedirect()}
-                                                            sx={{ backgroundColor: 'orange', m: 2, fontSize: '10px' }}  >Join Our Team</Button>
-                                                    </Grid>
-                                                )
-                                        })()
-                                    }  
-                                   
-
-                                  
-
-                                </div>
-                            </div>
-
-                        </Card>
-                       
-                    </div>
-                </div>
+  const navigate = useNavigate();
+  
+  return (
+    <div style={{ paddingTop: "1rem", justifyContent: "flex-start" }}>
+      <div className="d-flex justify-content-start">
+        <div>
+          <Card style={{ width: "30rem" }}>
+            <div
+              className="card text-start"
+              style={{ backgroundColor: "#673ab7" }}>
+              <Button
+                className="card-header"
+                style={{ fontSize: "25px", color: "white" }}
+                onClick={() => {
+                  navigate({
+                    pathname: "/TeamDetails",
+                    search: `?${createSearchParams({
+                      teamId: `${id}`,
+                      ownerID: `${ownerID}`,
+                    })}`,
+                  });
+                }}>
+                {name}
+              </Button>
             </div>
-
-        );
-    }
-
-
+          </Card>
+        </div>
+      </div>
+    </div>
+  );
+};
