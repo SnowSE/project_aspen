ae1b045592d4ed45f894cbc2e90f402f76c0a65c
Jayse19 <Jayse19@hotmail.com>
2023-03-21 13:44:56 -0600

    Made a new Icon for the teams page. Rough Work around for now but it works.

---
+++ src/combined/ClientApp/src/components/Share/ShareIconTeams.tsx
@@ -0,0 +1,28 @@
+﻿import { RWebShare } from "react-web-share";
+import ShareIcon from '@mui/icons-material/Share';
+import {
+    IconButton
+} from '@mui/material';
+
+const SharingIconTeams = () => {
+    const shareUrl = window.location.href;
+    return (
+        <div>
+            <RWebShare
+                data-testid={"shareModal"}
+                data={{
+                    text: "",
+                    url: shareUrl,
+                    title: "Name of Event Here"
+                }}
+                onClick={() => console.log("shared successfully!")}
+            >
+                <IconButton aria-label="delete" className="ShareIconTeams" size='large'>
+                    <ShareIcon />
+                </IconButton>
+            </RWebShare>
+        </div>
+    );
+};
+
+export default SharingIconTeams;
