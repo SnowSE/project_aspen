a808fdbff9012a84f5d10c18973afdcea94a31cf
Jesse Madsen <madsencad@gmail.com>
2023-03-27 13:26:21 -0600

    lint error fixed

--- src/combined/ClientApp/src/components/BlackTextProgressBar.tsx
+++ src/combined/ClientApp/src/components/BlackTextProgressBar.tsx
@@ -1,44 +1,53 @@
-﻿import * as React from 'react';
-import LinearProgress, { LinearProgressProps } from '@mui/material/LinearProgress';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import { useEffect } from 'react';
+import * as React from "react";
+import LinearProgress, {
+  LinearProgressProps,
+} from "@mui/material/LinearProgress";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import { useEffect } from "react";
 
-function LinearProgressWithLabel(props: LinearProgressProps & { value: number; goal: number }) {
-    return (
-        <Box>
-            <Box className="ProgressBarTextLocation" >
-                <Typography className="ProgressBarPercentageBlackTextDetails" >
-                    {`${Math.round(props.value)}%`}
-                </Typography>
+function LinearProgressWithLabel(
+  props: LinearProgressProps & { value: number; goal: number }
+) {
+  return (
+    <Box>
+      <Box className="ProgressBarTextLocation">
+        <Typography className="ProgressBarPercentageBlackTextDetails">
+          {`${Math.round(props.value)}%`}
+        </Typography>
 
-                <Typography className="ProgressBarBlackTextDetails">
-                    of our ${props.goal} dollar goal.
-                </Typography>
-                <br />
-            </Box>
-            <Box className="ProgressBarBarDetails">
-                <LinearProgress variant="determinate" {...props} />
-            </Box>
-        </Box>
-    );
+        <Typography className="ProgressBarBlackTextDetails">
+          of our ${props.goal} dollar goal.
+        </Typography>
+        <br />
+      </Box>
+      <Box className="ProgressBarBarDetails">
+        <LinearProgress variant="determinate" {...props} />
+      </Box>
+    </Box>
+  );
 }
 
 interface Props {
-    goalTotal: number;
-    currentTotal: number;
+  goalTotal: number;
+  currentTotal: number;
 }
 
 export default function BlackTextProgressBar(props: Props) {
-    const [progress, setProgress] = React.useState(1);
+  const [progress, setProgress] = React.useState(1);
 
-    useEffect(() => {
-        setProgress((props.currentTotal / props.goalTotal) * 100);
-    }, [props.goalTotal, props.currentTotal]);
+  useEffect(() => {
+    setProgress((props.currentTotal / props.goalTotal) * 100);
+  }, [props.goalTotal, props.currentTotal]);
 
-    return (
-        <Box className="ProgressBarTextStyling" >
-            <LinearProgressWithLabel className="ProgressBarDetails" color="success" value={progress} goal={props.goalTotal} />
-        </Box>
-    );
+  return (
+    <Box className="ProgressBarTextStyling">
+      <LinearProgressWithLabel
+        className="ProgressBarDetails"
+        color="success"
+        value={progress}
+        goal={props.goalTotal}
+      />
+    </Box>
+  );
 }
