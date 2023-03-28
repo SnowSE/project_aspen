abd50bcac14f0091e68e9af77fc2475c737026aa
Jayse19 <Jayse19@hotmail.com>
2023-03-28 15:40:11 -0600

    Back Button on Teams page, Whole card clickable, and nav bar has a teams link

--- src/combined/ClientApp/src/components/TeamsInfo/TeamDetails.tsx
+++ src/combined/ClientApp/src/components/TeamsInfo/TeamDetails.tsx
@@ -8,9 +8,9 @@ import { DonateButton } from "../DonateButton";
 import DynamicModal from "../DynamicModal";
 import { EventContext } from "../../App";
 import Team from "../../JsModels/team";
-import SharingIconTeams from "../Share/ShareIconTeams";
 import SharingButtonCustomLink from "../Share/SharingButtonCustomLink";
 import BlackTextProgressBar from "../BlackTextProgressBar";
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 
 
 export function TeamDetails() {
@@ -259,6 +259,13 @@ export function TeamDetails() {
     return (
         <Box>
             <Box>
+                <Box>
+                    <Button
+                        onClick={() => navigate("/TeamsListPage")}
+                    >
+                        <KeyboardBackspaceIcon className="BackToTeamsListButton" />
+                    </Button>
+                </Box>
                 <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
                     <Typography variant="h1">{currentTeam?.name} </Typography>
                     {
@@ -366,9 +373,6 @@ export function TeamDetails() {
                     )}
                 </Box>
                 <Box className="ShareIcon">
-                    <Box className="ShareIconTeams">
-                        <SharingIconTeams data-testid={"shareBtn"} />
-                    </Box>
                 </Box>
                 <Box className="DonateButtonPosition">
                     <DonateButton />
