e002f2120a1327489d3554ccf0cf93a0f6b4e6a1
Jesse Madsen <madsencad@gmail.com>
2023-03-29 10:04:41 -0600

    start

---
+++ src/combined/ClientApp/src/Donation/DonationAdminTable.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useTable } from 'react-table';
+
+const DonationTable = ({ columns, data }) => {
+    const {
+        getTableProps,
+        getTableBodyProps,
+        headerGroups,
+        rows,
+        prepareRow,
+    } = useTable({ columns, data });
+
+    return (
+        <table {...getTableProps()} style={{ border: 'solid 1px black' }}>
+            <thead>
+                {headerGroups.map(headerGroup => (
+                    <tr {...headerGroup.getHeaderGroupProps()}>
+                        {headerGroup.headers.map(column => (
+                            <th
+                                {...column.getHeaderProps()}
+                                style={{
+                                    borderBottom: 'solid 3px black',
+                                    background: 'aliceblue',
+                                    color: 'black',
+                                    fontWeight: 'bold',
+                                }}
+                            >
+                                {column.render('Header')}
+                            </th>
+                        ))}
+                    </tr>
+                ))}
+            </thead>
+            <tbody {...getTableBodyProps()}>
+                {rows.map(row => {
+                    prepareRow(row);
+                    return (
+                        <tr {...row.getRowProps()}>
+                            {row.cells.map(cell => {
+                                return (
+                                    <td
+                                        {...cell.getCellProps()}
+                                        style={{
+                                            padding: '10px',
+                                            border: 'solid 1px gray',
+                                            background: 'white',
+                                        }}
+                                    >
+                                        {cell.render('Cell')}
+                                    </td>
+                                );
+                            })}
+                        </tr>
+                    );
+                })}
+            </tbody>
+        </table>
+    );
+};
+
+export default DonationTable;
